5d140d59e4dadbb5796c1690744fa4614af8e70f
Graziano Giuliani <ggiulian@ictp.it>
2010-02-19 17:02:05 +0000

    Let us directly plot everything in the output directory...

--- regcm4/tests/plotgrads.sh
+++ regcm4/tests/plotgrads.sh
@@ -2,17 +2,21 @@
 
 if [ $# -ne 1 ]
 then
-  echo "Need input ctl filename"
+  echo "Need output directory name."
   exit 1
 fi
 
-ctlfile=$1
-varlist=`cat $ctlfile | awk 'BEGIN{found="0"} \
-                       {if ($1 == "vars") found="1"; \
-                        if ($1 == "endvars") found="0"; \
-                        if (found=="1") print $1}'`
+outdir=$1
 
-for var in $varlist
+for ctlfile in $outdir/*.ctl
 do
-  grads -blc "run grads/plotfinal.gs $ctlfile $var"
+  varlist=`cat $ctlfile | awk 'BEGIN{found="0"} \
+                         {if ($1 == "vars") found="1"; \
+                          if ($1 == "endvars") found="0"; \
+                          if (found=="1") print $1}'`
+
+  for var in $varlist
+  do
+    grads -blc "run grads/plotfinal.gs $ctlfile $var"
+  done
 done
