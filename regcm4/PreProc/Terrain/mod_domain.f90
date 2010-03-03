56603d6a6844ff877572bca922d7ba826f68dcda
Graziano Giuliani <ggiulian@ictp.it>
2010-03-03 09:56:16 +0000

    mod_domain.f90 with real variables in kind4

--- regcm4/PreProc/Terrain/mod_domain.f90
+++ regcm4/PreProc/Terrain/mod_domain.f90
@@ -4,25 +4,41 @@
 ! PARAMETER definitions
 !
       character(6) , parameter :: iproj = 'LAMCON'
-      integer , parameter :: iy = 34 , jx = 48 , kz = 18 , nsg = 1
-      real , parameter :: ds = 60.0 , ptop = 5.0 , clat = 45.39 ,       &
-                        & clon = 13.48 , plat = clat , plon = clon ,    &
-                        & truelatl = 30. , truelath = 60.
-      integer , parameter :: ntypec = 10 , ntypec_s = 10
-      real , parameter :: h2opct = 75.
-      logical , parameter :: ifanal = .true. , smthbdy = .false. ,      &
-                           & lakadj = .false.
-      integer , parameter :: igrads = 1 , ibigend = 1 , ibyte = 4
-      logical , parameter :: fudge_lnd = .false. ,                      &
-                           & fudge_lnd_s = .false. ,                    &
-                           & fudge_tex = .false. , fudge_tex_s = .false.
-      character(50) , parameter :: filout = '../../Input/DOMAIN.INFO' , &
-                                 & filctl = '../../Input/DOMAIN.CTL'
-      integer , parameter :: idate1 = 1990060100 , idate2 = 1990070100
-      character(5) , parameter :: dattyp = 'EIN15' , ssttyp = 'OI_WK'
+      integer , parameter :: iy = 34
+      integer , parameter :: jx = 48
+      integer , parameter :: kz = 18
+      integer , parameter :: nsg = 1
+      real(4) , parameter :: ds = 60.0
+      real(4) , parameter :: ptop = 5.0
+      real(4) , parameter :: clat = 45.39
+      real(4) , parameter :: clon = 13.48
+      real(4) , parameter :: plat = clat
+      real(4) , parameter :: plon = clon
+      real(4) , parameter :: truelatl = 30.
+      real(4) , parameter :: truelath = 60.
+      real(4) , parameter :: h2opct = 75.
+      integer , parameter :: ntypec = 10
+      integer , parameter :: ntypec_s = 10
+      logical , parameter :: ifanal = .true.
+      logical , parameter :: smthbdy = .false.
+      logical , parameter :: lakadj = .false.
+      integer , parameter :: igrads = 1
+      integer , parameter :: ibigend = 1
+      integer , parameter :: ibyte = 4
+      logical , parameter :: fudge_lnd = .false.
+      logical , parameter :: fudge_lnd_s = .false.
+      logical , parameter :: fudge_tex = .false.
+      logical , parameter :: fudge_tex_s = .false.
+      character(50) , parameter :: filout = '../../Input/DOMAIN.INFO'
+      character(50) , parameter :: filctl = '../../Input/DOMAIN.CTL'
+      integer , parameter :: idate1 = 1990060100
+      integer , parameter :: idate2 = 1990070100
+      character(5) , parameter :: dattyp = 'EIN15'
+      character(5) , parameter :: ssttyp = 'OI_WK'
       logical , parameter :: ehso4 = .false.
       character(4) , parameter :: lsmtyp = 'BATS'
       integer , parameter :: nveg = 20
       character(7) , parameter :: aertyp = 'AER00D0'
-      integer , parameter :: ntex = 17 , nproc = 16
+      integer , parameter :: ntex = 17
+      integer , parameter :: nproc = 16
       end module mod_domain
