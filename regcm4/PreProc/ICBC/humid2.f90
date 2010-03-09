2fafa26139233292c00e1e7810cbf8631b6f8530
Graziano Giuliani <ggiulian@ictp.it>
2010-03-09 11:33:00 +0000

    GPL header

--- regcm4/PreProc/ICBC/humid2.f90
+++ regcm4/PreProc/ICBC/humid2.f90
@@ -1,3 +1,22 @@
+!::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+!
+!    This file is part of RegCM model.
+!
+!    RegCM model is free software: you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+!
+!    RegCM model is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+!
+!    You should have received a copy of the GNU General Public License
+!    along with RegCM model.  If not, see <http://www.gnu.org/licenses/>.
+!
+!::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
       subroutine humid2(t,q,ps,pt,sigma,ni,nj,nk)
       use mod_constants , only : tzero , ep2 , lh0 , lh1 , lsvp1 , lsvp2
       implicit none
