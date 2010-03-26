3dcfbd31ea108515e965378041ae2d830bce5d8d
Graziano Giuliani <ggiulian@ictp.it>
2010-03-26 15:37:03 +0000

    GPL header

--- regcm4/PostProc/rdhead.f90
+++ regcm4/PostProc/rdhead.f90
@@ -1,3 +1,22 @@
+!::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+!
+!    This file is part of RegCM model.
+!
+!    RegCM model is free software: you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+!
+!    RegCM model is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+!
+!    You should have received a copy of the GNU General Public License
+!    along with RegCM model.  If not, see <http://www.gnu.org/licenses/>.
+!
+!::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
       subroutine rdhead(clat,clon,ds,pt,sigf,sigh,sighrev,xplat,xplon,f,&
                       & xmap,dmap,xlat,xlon,zs,zssd,ls,mdate0,&
                       & iin,inhead,idirect)
