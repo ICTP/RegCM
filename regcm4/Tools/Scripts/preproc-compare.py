6db8375e86c69451ef5ffb94123e642cbae962ea
Martin Scarcia <martin.scarcia@elettra.eu>
2010-05-21 16:02:56 +0000

    added gpl header to preproc-compare

--- regcm4/Tools/Scripts/preproc-compare.py
+++ regcm4/Tools/Scripts/preproc-compare.py
@@ -1,7 +1,29 @@
 #!/usr/bin/python
+
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#
+#    This file is part of ICTP RegCM.
+#
+#    ICTP RegCM is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    ICTP RegCM is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with ICTP RegCM.  If not, see <http://www.gnu.org/licenses/>.
+#
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
 # Script that evaluates differences between different
 # initial conditions for RegCM v3-v4 and produces
-# png images of the GrADS plots
+# png images of the GrADS plots by M. Scarcia
+
+# !!! Now working only for 1 month simulations !!!
 
 import os,sys,subprocess
 from optparse import OptionParser
