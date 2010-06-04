d83b51d75901ea46d61c1bc53e7c8d6498bb55c6
Graziano Giuliani <ggiulian@ictp.it>
2010-06-04 16:29:43 +0000

    Checked with gfortran

--- regcm4/PreProc/ICBC/icbc.f90
+++ regcm4/PreProc/ICBC/icbc.f90
@@ -228,7 +228,7 @@
           end if
           if ( igrads==1 ) call gradsctl(finame,idate,inmber)
           call fexist(finame)
-          open (64,file=finame,form='unformatted',status='repalce',     &
+          open (64,file=finame,form='unformatted',status='replace',     &
               & recl=jx*iy*ibyte,access='direct')
           ifile = ifile + 1
           noutrec = 0
