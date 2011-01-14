9dd3026452cc993a3a5fe548c599a8271271e56b
Graziano Giuliani <ggiulian@ictp.it>
2011-01-14 12:12:45 +0000

    More instrumentation.

--- PreProc/ICBC/mod_grid.f90
+++ PreProc/ICBC/mod_grid.f90
@@ -22,6 +22,8 @@
       use m_realkinds
       use m_die
       use m_stdio
+      use m_mall
+      use m_zeit
 
       real(sp) , allocatable , dimension(:,:) :: coriol , dlat , dlon , &
            & msfx , snowcv , topogm , xlandu , xlat , xlon
@@ -39,26 +41,67 @@
       subroutine init_grid(iy,jx,kz)
         implicit none
         integer , intent(in) :: iy , jx , kz
-        allocate(coriol(jx,iy))
-        allocate(dlat(jx,iy))
-        allocate(dlon(jx,iy))
-        allocate(msfx(jx,iy))
-        allocate(snowcv(jx,iy))
-        allocate(topogm(jx,iy))
-        allocate(xlandu(jx,iy))
-        allocate(xlat(jx,iy))
-        allocate(xlon(jx,iy))
-        allocate(pa(jx,iy))
-        allocate(sst1(jx,iy))
-        allocate(sst2(jx,iy))
-        allocate(tlayer(jx,iy))
-        allocate(za(jx,iy))
-        allocate(ice1(jx,iy))
-        allocate(ice2(jx,iy))
-        allocate(b3pd(jx,iy))
-        allocate(dsigma(kz))
-        allocate(sigma2(kz))
-        allocate(sigmaf(kz+1))
+        integer :: ierr
+        allocate(coriol(jx,iy), stat=ierr)
+        if (ierr /= 0) call die('init_grid','allocate coriol',ierr)
+        call mall_mci(coriol,'init_grid')
+        allocate(dlat(jx,iy), stat=ierr)
+        if (ierr /= 0) call die('init_grid','allocate dlat',ierr)
+        call mall_mci(dlat,'init_grid')
+        allocate(dlon(jx,iy), stat=ierr)
+        if (ierr /= 0) call die('init_grid','allocate dlon',ierr)
+        call mall_mci(dlon,'init_grid')
+        allocate(msfx(jx,iy), stat=ierr)
+        if (ierr /= 0) call die('init_grid','allocate msfx',ierr)
+        call mall_mci(msfx,'init_grid')
+        allocate(snowcv(jx,iy), stat=ierr)
+        if (ierr /= 0) call die('init_grid','allocate snowcv',ierr)
+        call mall_mci(snowcv,'init_grid')
+        allocate(topogm(jx,iy), stat=ierr)
+        if (ierr /= 0) call die('init_grid','allocate topogm',ierr)
+        call mall_mci(topogm,'init_grid')
+        allocate(xlandu(jx,iy), stat=ierr)
+        if (ierr /= 0) call die('init_grid','allocate xlandu',ierr)
+        call mall_mci(xlandu,'init_grid')
+        allocate(xlat(jx,iy), stat=ierr)
+        if (ierr /= 0) call die('init_grid','allocate xlat',ierr)
+        call mall_mci(xlat,'init_grid')
+        allocate(xlon(jx,iy), stat=ierr)
+        if (ierr /= 0) call die('init_grid','allocate xlon',ierr)
+        call mall_mci(xlon,'init_grid')
+        allocate(pa(jx,iy), stat=ierr)
+        if (ierr /= 0) call die('init_grid','allocate pa',ierr)
+        call mall_mci(pa,'init_grid')
+        allocate(sst1(jx,iy), stat=ierr)
+        if (ierr /= 0) call die('init_grid','allocate sst1',ierr)
+        call mall_mci(sst1,'init_grid')
+        allocate(sst2(jx,iy), stat=ierr)
+        if (ierr /= 0) call die('init_grid','allocate sst2',ierr)
+        call mall_mci(sst2,'init_grid')
+        allocate(tlayer(jx,iy), stat=ierr)
+        if (ierr /= 0) call die('init_grid','allocate tlayer',ierr)
+        call mall_mci(tlayer,'init_grid')
+        allocate(za(jx,iy), stat=ierr)
+        if (ierr /= 0) call die('init_grid','allocate za',ierr)
+        call mall_mci(za,'init_grid')
+        allocate(ice1(jx,iy), stat=ierr)
+        if (ierr /= 0) call die('init_grid','allocate ice1',ierr)
+        call mall_mci(ice1,'init_grid')
+        allocate(ice2(jx,iy), stat=ierr)
+        if (ierr /= 0) call die('init_grid','allocate ice2',ierr)
+        call mall_mci(ice2,'init_grid')
+        allocate(b3pd(jx,iy), stat=ierr)
+        if (ierr /= 0) call die('init_grid','allocate b3pd',ierr)
+        call mall_mci(b3pd,'init_grid')
+        allocate(dsigma(kz), stat=ierr)
+        if (ierr /= 0) call die('init_grid','allocate dsigma',ierr)
+        call mall_mci(dsigma,'init_grid')
+        allocate(sigma2(kz), stat=ierr)
+        if (ierr /= 0) call die('init_grid','allocate sigma2',ierr)
+        call mall_mci(sigma2,'init_grid')
+        allocate(sigmaf(kz+1), stat=ierr)
+        if (ierr /= 0) call die('init_grid','allocate sigmaf',ierr)
+        call mall_mci(sigmaf,'init_grid')
 
         call read_domain
 
@@ -66,25 +109,45 @@
 
       subroutine free_grid
         implicit none
+        call mall_mco(coriol,'init_grid')
         deallocate(coriol)
+        call mall_mco(dlat,'init_grid')
         deallocate(dlat)
+        call mall_mco(dlon,'init_grid')
         deallocate(dlon)
+        call mall_mco(msfx,'init_grid')
         deallocate(msfx)
+        call mall_mco(snowcv,'init_grid')
         deallocate(snowcv)
+        call mall_mco(topogm,'init_grid')
         deallocate(topogm)
+        call mall_mco(xlandu,'init_grid')
         deallocate(xlandu)
+        call mall_mco(xlat,'init_grid')
         deallocate(xlat)
+        call mall_mco(xlon,'init_grid')
         deallocate(xlon)
+        call mall_mco(pa,'init_grid')
         deallocate(pa)
+        call mall_mco(sst1,'init_grid')
         deallocate(sst1)
+        call mall_mco(sst2,'init_grid')
         deallocate(sst2)
+        call mall_mco(tlayer,'init_grid')
         deallocate(tlayer)
+        call mall_mco(za,'init_grid')
         deallocate(za)
+        call mall_mco(ice1,'init_grid')
         deallocate(ice1)
+        call mall_mco(ice2,'init_grid')
         deallocate(ice2)
+        call mall_mco(b3pd,'init_grid')
         deallocate(b3pd)
+        call mall_mco(dsigma,'init_grid')
         deallocate(dsigma)
+        call mall_mco(sigma2,'init_grid')
         deallocate(sigma2)
+        call mall_mco(sigmaf,'init_grid')
         deallocate(sigmaf)
       end subroutine free_grid
 
@@ -99,6 +162,7 @@
         character(256) :: fname
         integer :: jx_in , iy_in , kz_in , k
 
+        call zeit_ci('readdom')
         fname = trim(dirter)//pthsep//trim(domname)//'_DOMAIN000.nc'
 
         istatus = nf90_open(fname, nf90_nowrite, incin)
@@ -169,6 +233,7 @@
           dsigma(k) = sigmaf(k+1) - sigmaf(k)
         end do
 
+        call zeit_co('readdom')
       contains
 !
         subroutine check_ok(message)
