e8a113b9099712112ae8b7c72e0fd04d456c4582
Graziano Giuliani <ggiulian@ictp.it>
2011-04-01 10:46:22 +0000

    (trunk) : CDO modifies vertical dimension name. Fix plotting helpers for GrADS.

--- PostProc/GrADSNcPlot.F90
+++ PostProc/GrADSNcPlot.F90
@@ -52,7 +52,7 @@ program ncplot
   integer :: jx , iy , kz , nd , nt , nlat , nlon , ilat , ilon , isplit
   real(4) :: alat , alon , angle
   integer :: i , j
-  logical :: lvarsplit , lsigma , ldepth
+  logical :: lvarsplit , lsigma , lcdolev , ldepth
 #ifdef __PGI
   integer , external :: iargc
 #endif
@@ -63,6 +63,7 @@ program ncplot
   data cmon /'jan','feb','mar','apr','may','jun', &
              'jul','aug','sep','oct','nov','dec'/
   data lsigma /.true./
+  data lcdolev /.false./
   data ldepth /.false./
   data kzdimid  /-1/
   data itdimid  /-1/
@@ -148,6 +149,10 @@ program ncplot
   if (istatus /= nf90_noerr) then
     lsigma = .false.
     istatus = nf90_inq_dimid(ncid, "plev", kzdimid)
+    if (istatus /= nf90_noerr) then
+      istatus = nf90_inq_dimid(ncid, "lev", kzdimid)
+      if ( istatus == nf90_noerr) lcdolev = .true.
+    end if
   end if
   if (istatus == nf90_noerr) then
     istatus = nf90_inquire_dimension(ncid, kzdimid, len=kz)
@@ -388,7 +393,11 @@ program ncplot
     if (lsigma) then
       istatus = nf90_inq_varid(ncid, "sigma", ivarid)
     else
-      istatus = nf90_inq_varid(ncid, "plev", ivarid)
+      if (lcdolev) then
+        istatus = nf90_inq_varid(ncid, "lev", ivarid)
+      else
+        istatus = nf90_inq_varid(ncid, "plev", ivarid)
+      end if
     end if
     if (istatus /= nf90_noerr) then
       write (6,*) 'Error : level variable undefined'
