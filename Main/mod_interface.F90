2cf36687afbdfbbcfe0fc4c591e6c0f879057363
Ufuk Turunçoğlu <turuncu@ucar.edu>
2011-05-26 17:17:56 +0000

    (regcm-core) : First commit for ESMFized version of RegCM

---
+++ Main/mod_interface.F90
@@ -0,0 +1,484 @@
+      module mod_interface
+!
+!**********************************************************************
+!
+!     Used module declarations 
+!
+!**********************************************************************
+!
+      use mod_memutil
+      use mod_runparams
+      use mod_date
+      use mod_message
+      use mod_ncio
+      use mod_output
+      use mod_split
+      use mod_bdycod
+      use mod_che_semdde
+      use mod_init
+      use mod_header
+      use mod_param
+      use mod_tendency
+      use mod_tstep
+      use mod_service
+#ifdef CHEMTEST
+      use mod_chem
+#endif
+      use mod_mppio
+      use mpi
+#ifdef CLM
+      use perf_mod
+      use spmdMod, only: mpicom
+#endif
+      implicit none
+!
+      private
+      public :: RCM_initialize
+      public :: RCM_run
+      public :: RCM_finalize
+
+      contains
+ 
+      subroutine RCM_initialize(mpiCommunicator)
+!
+!**********************************************************************
+!
+!     Imported variable declarations 
+!
+!**********************************************************************
+!
+      integer, intent(in), optional :: mpiCommunicator
+!
+!**********************************************************************
+!
+!     Local variable declarations 
+!
+!**********************************************************************
+!
+      integer :: MyComm, ncpu, ibcdate, ierr
+      character(256) :: namelistfile, prgname
+! 
+!**********************************************************************
+!
+!**********************************************************************
+!
+!     MPI Initialization
+!
+!**********************************************************************
+!
+      if (present(mpiCommunicator)) then
+        MyComm = mpiCommunicator
+      else
+        MyComm = MPI_COMM_WORLD
+      end if
+      call mpi_comm_rank(MyComm, myid, ierr)
+      call mpi_comm_size(MyComm, ncpu, ierr)
+!
+      call whoami(myid)
+!
+#ifdef DEBUG 
+      call activate_debug()
+#endif
+!
+!**********************************************************************
+!
+!     Read input global namelist
+!
+!**********************************************************************
+!
+      if ( myid == 0 ) then
+        call getarg(0, prgname)
+        call getarg(1, namelistfile)
+        call initparam(namelistfile, ierr)
+        if ( ierr/=0 ) then
+          write ( 6, * ) 'Parameter initialization not completed'
+          write ( 6, * ) 'Usage : '
+          write ( 6, * ) '          ', trim(prgname), ' regcm.in'
+          write ( 6, * ) ' '
+          write ( 6, * ) 'Check argument and namelist syntax'
+          stop
+        end if
+!
+#ifdef BAND
+        call init_mod_ncio(.true.)
+#else
+        call init_mod_ncio(.false.)
+#endif
+!
+      end if
+
+      call memory_init
+!
+!**********************************************************************
+!
+!     MPI Initialization for model
+!
+!**********************************************************************
+!
+      call broadcast_params
+
+      call set_nproc(ncpu)
+
+      if ( ncpu /= nproc ) then
+        write (aline,*) 'The number of CPU is not well set'
+        call say
+        write (aline,*) 'NCPU = ' , ncpu , '    nproc =' , nproc
+        call say
+        call fatal(__FILE__,__LINE__,'CPU Count mismatch')
+      end if
+!      print * , "process" , myid , "of" , nproc
+      call mpi_barrier(mpi_comm_world,ierr)
+!     starttime= MPI_WTIME()
+      if ( myid > 0 ) then
+        iwest = myid - 1
+      else
+#ifdef BAND
+        iwest = nproc-1
+#else
+        iwest = mpi_proc_null
+#endif
+      end if
+      if ( myid < nproc-1 ) then
+        ieast = myid + 1
+      else
+#ifdef BAND
+        ieast = 0
+#else
+        ieast = mpi_proc_null
+#endif
+      end if
+      if ( jxp < 3 ) then
+        write (aline,*) 'The number of jxp must be greater than 2'
+        call say
+        write (aline,*) 'jxp = ' , jxp , '   jx = ' , jx
+        call say
+        call fatal(__FILE__,__LINE__,'Domain too small')
+      end if
+      if ( jxp*nproc /= jx ) then
+        write (aline,*) 'jx should be divided by nproc'
+        call say
+        write (aline,*) 'jx = ' , jx , '   nproc = ' , nproc
+        call say
+        call fatal(__FILE__,__LINE__,                                   &
+                 & 'Domain dimension not multiple of' //                &
+                 & ' processor number')
+      end if
+      jbegin = 1
+      jendl = jxp
+      jendx = jxp
+      jendm = jxp
+#ifndef BAND
+      if ( myid == 0 ) jbegin = 2
+      if ( myid == nproc-1 ) then
+        jendx = jxp - 1
+        jendm = jxp - 2
+      end if
+#endif
+!
+!**********************************************************************
+!
+!     RegCM V4 printout header
+!
+!**********************************************************************
+!
+      call header(myid,nproc)
+!
+!**********************************************************************
+!
+!     Parameter Setup
+!
+!**********************************************************************
+!
+      call param
+!
+      ibcdate = idatex
+!
+!**********************************************************************
+!
+!     Read initial data
+!
+!**********************************************************************
+!
+! this below enable debugging
+#ifdef DEBUG 
+      call start_debug()
+#endif 
+!
+      call init
+#ifdef CHEMTEST
+      if ( ichem == 1 ) then
+        call init_chem
+      end if
+#endif
+!
+!**********************************************************************
+!
+!     Read Boundary conditions
+!
+!**********************************************************************
+!
+      call bdyin
+#ifdef CHEMTEST
+      if ( ichem == 1 ) then
+        call bdyin_chem
+      end if
+#endif
+      call addhours(ibcdate,ibdyfrq)
+!
+      call spinit
+! 
+      if ( ichem == 1 ) call chsrfem
+!
+!**********************************************************************
+!
+!     Write initial state to output
+!
+!**********************************************************************
+!
+      call output
+!
+!**********************************************************************
+!
+!     Clean up and logging
+!
+!**********************************************************************
+!
+      call free_mpp_initspace
+      call time_print(6,'inizialization phase')
+      call time_reset()
+!
+      return
+      end subroutine RCM_initialize
+!
+!=======================================================================
+!                                                                      !
+!     This routine runs RegCM model from specified starting (TimeStr)  !
+!     to ending (TimeEnd) time-steps.                                  !
+!                                                                      !
+!=======================================================================
+!
+      subroutine RCM_run(timestr, timeend, first)
+      implicit none
+!
+!**********************************************************************
+!
+!     Imported variable declarations 
+!
+!**********************************************************************
+!
+      real(8), intent(in) :: timestr   ! starting time-step
+      real(8), intent(in) :: timeend   ! ending   time-step
+      logical, intent(in) :: first
+!
+!**********************************************************************
+!
+!     Local variable declarations 
+!
+!**********************************************************************
+!
+      real(8) :: dtinc, extime
+      integer :: iexec
+      integer :: ibcdate
+!
+!**********************************************************************
+!
+!     Initialization of parameters 
+!
+!**********************************************************************
+!
+      if ( first ) then
+        extime = 0.0D0
+        iexec  = 1
+        dtinc  = dt
+        ibcdate = idatex 
+      end if
+!
+!**********************************************************************
+!
+!     Model run 
+!
+!**********************************************************************
+!     
+      do while ( extime >= timestr .and. extime < timeend)
+!
+!       Read in boundary conditions if needed
+!
+        if ( idatex == ibcdate .and. idatex < idate2 ) then
+          call bdyin
+#ifdef CHEMTEST
+          if ( ichem == 1 ) call bdyin_chem
+#endif
+          call addhours(ibcdate,ibdyfrq)
+        end if
+!
+!       Refined start
+!
+        if ( .not.ifrest ) then
+          if ( rfstrt ) then
+            if ( (jyear == jyear0 .and. ktau == 0) .or.                 &
+               & dtinc /= deltmx ) then
+              call tstep(extime,dtinc,deltmx)
+              write (aline, 99001) extime , dtinc , dt , dt2 ,          &
+                                   & dtmin , ktau , jyear
+              call say
+            end if
+          end if
+        end if
+!
+!       Compute tendencies
+!
+        call tend(iexec)
+!
+!       Split modes
+!
+        call splitf
+!
+!       Write output for this timestep if requested
+!
+        if (ifrest) done_restart = .true.
+        call output
+!
+!       Increment time
+!
+        extime = extime + dtinc
+        if (debug_level > 3) then
+          if (myid == 0) write(6,'(a,i10,a,i10,a)') &
+             'Simulation time: ', idatex , '+', &
+             idint(dmod(extime,secph)), ' h'
+        end if
+!
+      end do
+
+!this below close down debug 
+#ifdef DEBUG
+      call stop_debug()
+#endif 
+      call time_print(6,'evolution phase')
+!
+!**********************************************************************
+!
+!     Formats
+!
+!**********************************************************************
+!
+99001 format (6x,'large domain: extime = ',f7.1,' dtinc = ',f7.1,       &
+            & ' dt = ',f7.1,' dt2 = ',f7.1,' dtmin = ',f6.1,' ktau = ', &
+            & i7,' in year ',i4)
+
+      end subroutine RCM_run
+
+      subroutine RCM_finalize
+      implicit none
+!
+!**********************************************************************
+!
+!     Local variable declarations 
+!
+!**********************************************************************
+!
+      integer :: nhours, ierr
+!
+!**********************************************************************
+!
+!     Simulation completed
+!
+!**********************************************************************
+!
+      call release_mod_ncio
+!
+      write (aline, 99002) xtime , ktau , jyear
+      call say
+!
+!**********************************************************************
+!
+!     Set length of next run (auto-restart option)
+!
+!**********************************************************************
+!
+      nhours = idatediff(idate2,idate1)
+      idate1 = idate2
+      call addhours(idate2,nhours)
+      write (aline, *) ' *** new max DATE will be ' , idate2
+      call say
+!
+!**********************************************************************
+!
+!     Finalize the components 
+!
+!**********************************************************************
+!
+      if ( myid == 0 ) then
+        call for_next
+      end if
+      call mpi_finalize(ierr)
+#ifdef CLM
+      call t_prf('timing_all',mpicom)
+      call t_finalizef()
+#endif
+!
+      call memory_destroy
+      call finaltime(myid)
+!
+      if ( myid == 0 ) then
+        print *, 'RegCM V4 simulation successfully reached end'
+      end if
+!
+!**********************************************************************
+!
+!     Formats
+!
+!**********************************************************************
+!
+99002 format (                                                          &
+         & ' ***** restart file for next run is written at time     = ',&
+         & f10.2,' minutes, ktau = ',i7,' in year ',i4)
+!
+      end subroutine RCM_finalize
+!
+!**********************************************************************
+!
+!    Subroutine to write file restparam.nl with an hint 
+!    for restarting the model
+!
+!**********************************************************************
+!
+      subroutine for_next
+      implicit none
+! 
+!**********************************************************************
+!
+!     Open file and write data
+!
+!**********************************************************************
+!
+      open(99, file='restparam.nl')
+      write (99,99001) '&restartparam'
+      if ( idate1 < globidate2 ) then
+        write (99,99001) 'ifrest  = .true. '
+      else
+        write (99,99001) 'ifrest  = .false.'
+      end if
+      write (99,99002) 'idate0  = ' , idate0
+      write (99,99002) 'idate1  = ' , idate1
+      write (99,99002) 'idate2  = ' , globidate2
+      write (99,99002) '/'
+! 
+!**********************************************************************
+!
+!     Close file
+!
+!**********************************************************************
+!
+      close (99)
+!
+!**********************************************************************
+!
+!     Formats
+!
+!**********************************************************************
+!
+99001 format (1x,a)
+99002 format (1x,a,i10,',')
+!
+      end subroutine for_next
+      end module mod_interface
---
+++ Main/regcm_esmf.h
@@ -0,0 +1,194 @@
+      program regcm
+!
+!**********************************************************************
+!
+!     Used module declarations 
+!
+!**********************************************************************
+!
+      use ESMF_Mod
+
+      use mod_coupler
+      use mod_esmf
+!
+!**********************************************************************
+!
+!     Local variable declarations 
+!
+!**********************************************************************
+!
+      type(ESMF_VM) :: vm
+      type(ESMF_GridComp) :: gcomp
+      character(ESMF_MAXSTR), parameter :: conv = 'ESMF'
+      character(ESMF_MAXSTR), parameter :: purp = 'General'
+!
+      type(ESMF_Time) :: time
+      type(ESMF_TimeInterval) :: timeInt 
+!
+      integer :: status, rank, npet, comm
+      character (len=80) :: name, importName, exportName
+!
+!**********************************************************************
+!
+!     Model Initialization
+!
+!**********************************************************************
+!
+      call ESMF_Initialize(vm=vm,                                       &
+     &                     rc=status)
+!
+      call ESMF_VMGet(vm,                                               &
+     &                localPet=rank,                                    &
+     &                petCount=npet,                                    &
+     &                mpiCommunicator=comm,                             &
+     &                rc=status)
+!
+      call allocate_cplvars(npet)
+!
+!**********************************************************************
+!
+!     Create gridded components
+!
+!**********************************************************************
+!
+      do i = 1, nModels 
+        if (i == 1) then
+          name = 'atmos' 
+        else if (i == 2) then
+          name = 'ocean'
+        end if
+!
+        models(i)%gcomp = ESMF_GridCompCreate(name=TRIM(name),          &
+     &                                        petList=models(i)%petList,&
+     &                                        rc=status)
+      end do
+!
+!**********************************************************************
+!
+!     Add attribute to gridded components and print out
+!
+!**********************************************************************
+!
+      do i = 1, nModels
+!
+!       Add attribute
+!
+        call ESMF_AttributeAdd(models(i)%gcomp,                         &
+     &                         convention=conv,                         &
+     &                         purpose=purp,                            &
+     &                         rc=status)
+!
+!       Set attributes
+!
+        do j = 1, size(models(i)%attName)
+          call ESMF_AttributeSet(models(i)%gcomp,                       &
+     &                           name=models(i)%attName(j),             &
+     &                           value=models(i)%attValue(j),           &
+     &                           convention=conv,                       &
+     &                           purpose=purp,                          &
+     &                           rc=status)
+        end do
+!
+!       Print attributes
+!
+        call ESMF_AttributeWrite(models(i)%gcomp,                       &
+     &                           convention=conv,                       &
+     &                           purpose=purp,                          &
+     &                           attwriteflag=ESMF_ATTWRITE_XML,        &
+     &                           rc=status)
+      end do
+!
+!**********************************************************************
+!
+!     Register gridded components
+!
+!**********************************************************************
+!
+      call ESMF_GridCompSetServices(models(Iatmos)%gcomp,              &
+     &                              RCM_SetServices,                   &
+     &                              status)
+!
+      call ESMF_GridCompSetServices(models(Iocean)%gcomp,              &
+     &                              ROMS_SetServices,                  &
+     &                              status)
+!
+!**********************************************************************
+!
+!     Create gridded components export/import state objects 
+!
+!**********************************************************************
+!
+      do i = 1, nModels
+        if (i == 1) then
+          importName = 'Atmosphere gridded component import state'
+          exportName = 'Atmosphere gridded component export state'
+        else if (i == 2) then
+          importName = 'Ocean gridded component import state'
+          exportName = 'Ocean gridded component export state'
+        end if
+!
+        models(i)%stateExport = ESMF_StateCreate(trim(exportName),     &
+     &                                           ESMF_STATE_EXPORT,    &
+     &                                           rc=status)
+!
+        models(i)%stateImport = ESMF_StateCreate(trim(importName),     &
+     &                                           ESMF_STATE_IMPORT,    &
+     &                                           rc=status)
+      end do
+!
+!**********************************************************************
+!
+!     Initialize gridded components 
+!
+!**********************************************************************
+!
+      do i = 1, nModels
+        call ESMF_GridCompInitialize(models(i)%gcomp,                  &
+     &                               importState=models(i)%stateImport,&
+     &                               exportState=models(i)%stateExport,&
+     &                               rc=status)  
+!
+!       Print time information (for debug purposes)
+!
+        if (rank == models(i)%petList(1)) then
+          call ESMF_ClockGet(models(i)%clock,                          &
+     &                       startTime=time,                           & 
+     &                       timeStep=timeInt)
+          call ESMF_TimePrint(time,                                    &
+     &                        options="string",                        &
+     &                        rc=status)
+          call ESMF_TimeIntervalPrint(timeInt,                         &
+     &                                options="string",                &
+     &                                rc=status)
+        end if
+      end do
+!
+!**********************************************************************
+!
+!      Set time interval to exchange data between gridded components 
+!
+!**********************************************************************
+!
+!      do i = 1, nModels 
+!
+!      end do
+!
+!      call ESMF_TimeIntervalSet(TimeStep(0),                            &
+!     &                          s_r8=TimeInterval(Iocean,Iatmos),       &
+!     &                          rc=status)
+!
+!
+!      first = .TRUE.
+!      timestr = 0.0
+!      timeend = idatediff(idate2,idate1)*3600.0
+!      call RCM_run(timestr, timeend, first)
+!
+!**********************************************************************
+!
+!     Model Finalize 
+!
+!**********************************************************************
+!
+!       call RCM_finalize()
+!
+       end program regcm
