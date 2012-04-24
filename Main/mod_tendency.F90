9e4978dddfc5e9814ced7e5a7e014a956c2736c6
Dr Fabien Solmon <fabien.solmon@aero.obs-mip.fr>
2012-04-24 17:06:13 +0000

    calday in tractend call

--- Main/mod_tendency.F90
+++ Main/mod_tendency.F90
@@ -714,7 +714,7 @@ module mod_tendency
       ! Compute chemistry tendencies (other than transport)
       !
       sod = dble(idatex%second_of_day)
-      call tractend2(ktau,xyear,xmonth,xday,sod)
+      call tractend2(ktau,xyear,xmonth,xday,sod,calday)
       !
     end if ! ichem
 !
