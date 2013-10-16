811e745e3c9e5b5242110dc4946d4063c846d8b8
Graziano Giuliani <ggiulian@ictp.it>
2013-10-16 10:33:52 +0000

    (regcm-core) : revamp R example to use PCICt to handle multiple calendars instead of udunits2 and chron (was limited to gregorian)

--- Tools/Scripts/R/tprplot.r
+++ Tools/Scripts/R/tprplot.r
@@ -18,9 +18,8 @@
 #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 library("ncdf")
-library("udunits2")
+library("PCICt")
 library("fields")
-library("chron")
 
 surface = open.ncdf("/scratch/ggiulian/test/output/verysmall_SRF.1999050100.nc")
 lonmat  = get.var.ncdf(nc=surface,varid="xlon")
@@ -28,16 +27,18 @@ latmat  = get.var.ncdf(nc=surface,varid="xlat")
 
 timearr   = get.var.ncdf(nc=surface,varid="time")
 timeunits = att.get.ncdf(nc=surface,varid="time",attname="units")
+calendar  = att.get.ncdf(nc=surface,varid="time",attname="calendar")
 
 tpr       = get.var.ncdf(nc=surface,varid="pr")
 tprunits  = att.get.ncdf(nc=surface,varid="pr",attname="units")
 
-ts = ud.convert(timearr,timeunits$value,"seconds since 1970-01-01 00:00:00 GMT")
-a = as.POSIXct(ts,origin="1970-01-01 00:00:00 GMT",tz="GMT")
+seconds_per_hour <- 3600.0
+origin <- as.PCICt(substring(timeunits$value,13,31),cal=calendar$value)
+times <- origin + timearr*seconds_per_hour
 
 inds = (1:dim(timearr))
-targettime = chron(dates("1999-05-05",format="y-m-d"), times("21:00:00"))
-tind = inds[as.chron(a) == targettime]
+targettime = as.PCICt("1999-05-06 12:00:00",cal=calendar$value)
+tind = inds[times == targettime]
 
 image.plot(lonmat, latmat,tpr[,,tind])
 
