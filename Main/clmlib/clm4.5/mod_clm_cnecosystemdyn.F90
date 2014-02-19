4682d05058809a067189928970db8733c8f5e16f
Graziano Giuliani <ggiulian@ictp.it>
2014-02-19 14:24:29 +0000

    (regcm-core) : CLM

--- Main/clmlib/clm4.5/mod_clm_cnecosystemdyn.F90
+++ Main/clmlib/clm4.5/mod_clm_cnecosystemdyn.F90
@@ -3,8 +3,39 @@ module mod_clm_cnecosystemdyn
   !
   ! Ecosystem dynamics: phenology, vegetation
   !
+  use mod_intkinds
   use mod_realkinds
-  use mod_clm_varctl  , only: fpftdyn, use_c13, use_c14
+  use mod_clm_type
+  use mod_clm_varctl , only: fpftdyn, use_c13, use_c14
+  use mod_clm_cnallocation , only : CNAllocationInit
+  use mod_clm_cnphenology , only : CNPhenologyInit
+  use mod_clm_cnfire , only : CNFireInit
+  use mod_clm_cnc14decay , only : C14_init_BombSpike
+  use mod_clm_surfrd , only : crop_prog
+  use mod_clm_cnsetvalue , only : CNZeroFluxes
+  use mod_clm_cnndynamics , only : CNNDeposition , CNNFixation
+  use mod_clm_cnndynamics , only : CNNLeaching, CNNFert, CNSoyfix
+  use mod_clm_cnmresp , only : CNMResp
+  use mod_clm_cndecomp , only : CNDecompAlloc
+  use mod_clm_cnphenology , only : CNPhenology
+  use mod_clm_cngresp , only : CNGResp
+  use mod_clm_cncstateupdate1 , only : CStateUpdate1,CStateUpdate0
+  use mod_clm_cnnstateupdate1 , only : NStateUpdate1
+  use mod_clm_cngapmortality , only : CNGapMortality
+  use mod_clm_cncstateupdate2 , only : CStateUpdate2, CStateUpdate2h
+  use mod_clm_cnnstateupdate2 , only : NStateUpdate2, NStateUpdate2h
+  use mod_clm_cnfire , only : CNFireArea, CNFireFluxes
+  use mod_clm_cncstateupdate3 , only : CStateUpdate3
+  use mod_clm_cnnstateupdate3 , only : NStateUpdate3
+  use mod_clm_cnprecisioncontrol , only : CNPrecisionControl
+  use mod_clm_cnvegstructupdate , only : CNVegStructUpdate
+  use mod_clm_cnannualupdate , only : CNAnnualUpdate
+  use mod_clm_cnsummary , only : CSummary, NSummary
+  use mod_clm_cncisoflux , only : CIsoFlux1, CIsoFlux2, CIsoFlux2h, CIsoFlux3
+  use mod_clm_cnc14decay , only : C14Decay, C14BombSpike
+  use mod_clm_pftdyn , only : CNHarvest
+  use mod_clm_cnwoodproducts , only : CNWoodProducts
+  use mod_clm_cnsoillittverttransp , only : CNSoilLittVertTransp
 
   implicit none
 
@@ -18,10 +49,6 @@ module mod_clm_cnecosystemdyn
   ! Initialzation of the CN Ecosystem dynamics.
   !
   subroutine CNEcosystemDynInit(lbg, ubg, lbc, ubc, lbp, ubp )
-    use mod_clm_cnallocation, only : CNAllocationInit
-    use mod_clm_cnphenology , only : CNPhenologyInit
-    use mod_clm_cnfire , only : CNFireInit
-    use mod_clm_cnc14decay  , only : C14_init_BombSpike
     implicit none
     integer(ik4) , intent(in) :: lbg , ubg        ! gridcell bounds
     integer(ik4) , intent(in) :: lbc , ubc        ! column bounds
@@ -39,216 +66,169 @@ module mod_clm_cnecosystemdyn
   !
   subroutine CNEcosystemDyn(lbc, ubc, lbp, ubp, num_soilc, filter_soilc, &
                      num_soilp, filter_soilp, num_pcropp, filter_pcropp, doalb)
-! !USES:
-    use clmtype
-    use spmdMod              , only: masterproc
-    use CNSetValueMod        , only: CNZeroFluxes
-    use CNNDynamicsMod       , only: CNNDeposition,CNNFixation, CNNLeaching, CNNFert, CNSoyfix
-    use CNMRespMod           , only: CNMResp
-    use CNDecompMod          , only: CNDecompAlloc
-    use CNPhenologyMod       , only: CNPhenology
-    use CNGRespMod           , only: CNGResp
-    use CNCStateUpdate1Mod   , only: CStateUpdate1,CStateUpdate0
-    use CNNStateUpdate1Mod   , only: NStateUpdate1
-    use CNGapMortalityMod    , only: CNGapMortality
-    use CNCStateUpdate2Mod   , only: CStateUpdate2, CStateUpdate2h
-    use CNNStateUpdate2Mod   , only: NStateUpdate2, NStateUpdate2h
-    use CNFireMod            , only: CNFireArea, CNFireFluxes
-    use CNCStateUpdate3Mod   , only: CStateUpdate3
-    use CNNStateUpdate3Mod   , only: NStateUpdate3
-    use CNPrecisionControlMod, only: CNPrecisionControl
-    use CNVegStructUpdateMod , only: CNVegStructUpdate
-    use CNAnnualUpdateMod    , only: CNAnnualUpdate
-    use CNSummaryMod         , only: CSummary, NSummary
-    use CNCIsoFluxMod        , only: CIsoFlux1, CIsoFlux2, CIsoFlux2h, CIsoFlux3
-    use CNC14DecayMod        , only: C14Decay, C14BombSpike
-    use pftdynMod              , only: CNHarvest
-    use CNWoodProductsMod      , only: CNWoodProducts
-    use CNSoilLittVertTranspMod, only: CNSoilLittVertTransp
-    use perf_mod               , only: t_startf, t_stopf
-    use surfrdMod              , only: crop_prog
-    use shr_sys_mod            , only: shr_sys_flush
-!
-! !ARGUMENTS:
     implicit none
-    integer(ik4), intent(in) :: lbc, ubc        ! column bounds
-    integer(ik4), intent(in) :: lbp, ubp        ! pft bounds
-    integer(ik4), intent(in) :: num_soilc       ! number of soil columns in filter
-    integer(ik4), intent(in) :: filter_soilc(ubc-lbc+1) ! filter for soil columns
-    integer(ik4), intent(in) :: num_soilp       ! number of soil pfts in filter
-    integer(ik4), intent(in) :: filter_soilp(ubp-lbp+1) ! filter for soil pfts
-    integer(ik4), intent(in) :: num_pcropp      ! number of prog. crop pfts in filter
-    integer(ik4), intent(in) :: filter_pcropp(ubp-lbp+1)! filter for prognostic crop pfts
-    logical, intent(in) :: doalb           ! true = surface albedo calculation time step
-!
-! !CALLED FROM:
-!
-! !REVISION HISTORY:
-! 10/22/03, Peter Thornton: created from EcosystemDyn during migration to
-!                           new vector code.
-! 11/3/03, Peter Thornton: removed update of elai, esai, frac_veg_nosno_alb.
-!     These are now done in CNVegStructUpdate(), which is called
-!     prior to SurfaceAlbedo().
-! 11/13/03, Peter Thornton: switched from nolake to soil filtering.
-!
-! !LOCAL VARIABLES:
-!
-! local pointers to implicit in arguments
-!
-! local pointers to implicit out arguments
-!
-! !OTHER LOCAL VARIABLES:
-!
-!EOP
-!-----------------------------------------------------------------------
+    integer(ik4), intent(in) :: lbc, ubc ! column bounds
+    integer(ik4), intent(in) :: lbp, ubp ! pft bounds
+    integer(ik4), intent(in) :: num_soilc ! number of soil columns in filter
+    ! filter for soil columns
+    integer(ik4), intent(in) :: filter_soilc(ubc-lbc+1)
+    integer(ik4), intent(in) :: num_soilp ! number of soil pfts in filter
+    ! filter for soil pfts
+    integer(ik4), intent(in) :: filter_soilp(ubp-lbp+1)
+    ! number of prog. crop pfts in filter
+    integer(ik4), intent(in) :: num_pcropp
+    ! filter for prognostic crop pfts
+    integer(ik4), intent(in) :: filter_pcropp(ubp-lbp+1)
+    ! true = surface albedo calculation time step
+    logical, intent(in) :: doalb
 
- !   if (doalb) then
+    ! Call the main CN routines
+    call CNZeroFluxes(num_soilc, filter_soilc, num_soilp, filter_soilp)
 
-       ! Call the main CN routines
-       call t_startf('CNZero')
-       call CNZeroFluxes(num_soilc, filter_soilc, num_soilp, filter_soilp)
-       call t_stopf('CNZero')
+    call CNNDeposition(lbc, ubc)
 
-       call t_startf('CNDeposition')
-       call CNNDeposition(lbc, ubc)
-       call t_stopf('CNDeposition')
+    call CNNFixation(num_soilc,filter_soilc)
 
-       call t_startf('CNFixation')
-       call CNNFixation(num_soilc,filter_soilc)
-       call t_stopf('CNFixation')
-
-       call t_startf('CNMResp')
-       if (crop_prog) call CNNFert(num_soilc,filter_soilc)
-
-       if (crop_prog) call CNSoyfix(num_soilc, filter_soilc, num_soilp, filter_soilp)
-
-       call CNMResp(lbc, ubc, num_soilc, filter_soilc, num_soilp, filter_soilp)
-       call t_stopf('CNMResp')
-
-       call t_startf('CNDecompAlloc')
-       call CNDecompAlloc(lbp, ubp, lbc, ubc, num_soilc, filter_soilc, &
-                          num_soilp, filter_soilp )
-       call t_stopf('CNDecompAlloc')
-
-       ! CNphenology needs to be called after CNdecompAlloc, because it
-       ! depends on current time-step fluxes to new growth on the last
-       ! litterfall timestep in deciduous systems
-
-       call t_startf('CNPhenology')
-       call CNPhenology(num_soilc, filter_soilc, num_soilp, filter_soilp, &
-                        num_pcropp, filter_pcropp, doalb)
-       call t_stopf('CNPhenology')
-
-       call t_startf('CNUpdate0')
-       call CNGResp(num_soilp, filter_soilp)
-       
-       call CStateUpdate0(num_soilp, filter_soilp, 'bulk')
-
-       if ( use_c13 ) call CStateUpdate0(num_soilp, filter_soilp, 'c13')
-       call t_stopf('CNUpdate0')
-
-       call t_startf('CNUpdate1')
-       if ( use_c13 ) call CIsoFlux1(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c13')
-
-       if ( use_c14 ) call CStateUpdate0(num_soilp, filter_soilp, 'c14')
-
-       if ( use_c14 ) call CIsoFlux1(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c14')
-
-       call CStateUpdate1(num_soilc, filter_soilc, num_soilp, filter_soilp, 'bulk')
-
-       if ( use_c13 ) call CStateUpdate1(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c13')
-       
-       if ( use_c14 ) call CStateUpdate1(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c14')
-       
-       call NStateUpdate1(num_soilc, filter_soilc, num_soilp, filter_soilp)
-
-       call t_startf('CNSoilLittVertTransp')
-       call CNSoilLittVertTransp(lbc, ubc, num_soilc, filter_soilc)
-       call t_stopf('CNSoilLittVertTransp')
-
-       call t_startf('CNGapMortality')
-       call CNGapMortality(num_soilc, filter_soilc, num_soilp, filter_soilp)
-       call t_stopf('CNGapMortality')
-
-       call t_stopf('CNUpdate1')
-
-       call t_startf('CNUpdate2')
-       if ( use_c13 ) call CIsoFlux2(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c13')
-
-       if ( use_c14 ) call CIsoFlux2(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c14')
-
-       call CStateUpdate2(num_soilc, filter_soilc, num_soilp, filter_soilp, 'bulk')
-
-       if ( use_c13 ) call CStateUpdate2(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c13')
-
-       if ( use_c14 ) call CStateUpdate2(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c14')
-
-       call NStateUpdate2(num_soilc, filter_soilc, num_soilp, filter_soilp)
-       
-       if (fpftdyn /= ' ') then
-          call CNHarvest(num_soilc, filter_soilc, num_soilp, filter_soilp)
-       end if 
-
-       if ( use_c13 ) call CIsoFlux2h(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c13')
-
-       if ( use_c14 ) call CIsoFlux2h(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c14')
-
-       call CStateUpdate2h(num_soilc, filter_soilc, num_soilp, filter_soilp, 'bulk')
-
-       if ( use_c13 ) call CStateUpdate2h(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c13')
-
-       if ( use_c14 ) call CStateUpdate2h(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c14')
-
-       call NStateUpdate2h(num_soilc, filter_soilc, num_soilp, filter_soilp)
-       
-       call CNWoodProducts(num_soilc, filter_soilc)
-       
-       call CNFireArea(num_soilc, filter_soilc,num_soilp, filter_soilp)
-
-       call CNFireFluxes(num_soilc, filter_soilc, num_soilp, filter_soilp)
-
-       call CNNLeaching(lbc, ubc, num_soilc, filter_soilc)
-       call t_stopf('CNUpdate2')
-
-       call t_startf('CNUpdate3')
-       if ( use_c13 ) call CIsoFlux3(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c13')
-
-       if ( use_c14 ) call CIsoFlux3(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c14')
-
-       call CStateUpdate3(num_soilc, filter_soilc, num_soilp, filter_soilp, 'bulk')
-
-       if ( use_c13 ) call CStateUpdate3(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c13')
-
-       if ( use_c14 ) call CStateUpdate3(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c14')
-
-       if ( use_c14 ) call C14Decay(num_soilc, filter_soilc, num_soilp, filter_soilp)
-
-       if ( use_c14 ) call C14BombSpike(num_soilp, filter_soilp)
-
-       call NStateUpdate3(num_soilc, filter_soilc, num_soilp, filter_soilp)
-       call t_stopf('CNUpdate3')
-
-       call t_startf('CNsum')
-       call CNPrecisionControl(num_soilc, filter_soilc, num_soilp, filter_soilp)
-
-    if (doalb) then   
-       call CNVegStructUpdate(num_soilp, filter_soilp)
+    if (crop_prog) then
+      call CNNFert(num_soilc,filter_soilc)
+      call CNSoyfix(num_soilc, filter_soilc, num_soilp, filter_soilp)
     end if
 
-!       call CNAnnualUpdate(num_soilc, filter_soilc, num_soilp, filter_soilp)
-       
-       call CSummary(num_soilc, filter_soilc, num_soilp, filter_soilp, 'bulk')
-       
-       if ( use_c13 ) call CSummary(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c13')
+    call CNMResp(lbc, ubc, num_soilc, filter_soilc, num_soilp, filter_soilp)
 
-       if ( use_c14 ) call CSummary(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c14')
-       
-       call NSummary(num_soilc, filter_soilc, num_soilp, filter_soilp)
-       call t_stopf('CNsum')
+    call CNDecompAlloc(lbp, ubp, lbc, ubc, num_soilc, filter_soilc, &
+                          num_soilp, filter_soilp )
 
-!    end if  !end of if-doalb block
+    ! CNphenology needs to be called after CNdecompAlloc, because it
+    ! depends on current time-step fluxes to new growth on the last
+    ! litterfall timestep in deciduous systems
+
+    call CNPhenology(num_soilc, filter_soilc, num_soilp, filter_soilp, &
+                        num_pcropp, filter_pcropp, doalb)
+
+    call CNGResp(num_soilp, filter_soilp)
+       
+    call CStateUpdate0(num_soilp, filter_soilp, 'bulk')
+
+    if ( use_c13 ) then
+      call CStateUpdate0(num_soilp, filter_soilp, 'c13')
+      call CIsoFlux1(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c13')
+    end if
+
+    if ( use_c14 ) then
+      call CStateUpdate0(num_soilp, filter_soilp, 'c14')
+      call CIsoFlux1(num_soilc,filter_soilc,num_soilp,filter_soilp,'c14')
+      call CStateUpdate1(num_soilc,filter_soilc,num_soilp,filter_soilp,'bulk')
+    end if
+
+    if ( use_c13 ) then
+      call CStateUpdate1(num_soilc,filter_soilc,num_soilp,filter_soilp,'c13')
+    end if
+    if ( use_c14 ) then
+      call CStateUpdate1(num_soilc,filter_soilc,num_soilp,filter_soilp,'c14')
+    end if
+       
+    call NStateUpdate1(num_soilc, filter_soilc, num_soilp, filter_soilp)
+
+    call CNSoilLittVertTransp(lbc, ubc, num_soilc, filter_soilc)
+
+    call CNGapMortality(num_soilc, filter_soilc, num_soilp, filter_soilp)
+
+    if ( use_c13 ) then
+      call CIsoFlux2(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c13')
+    end if
+
+    if ( use_c14 ) then
+      call CIsoFlux2(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c14')
+    end if
+
+    call CStateUpdate2(num_soilc, filter_soilc, num_soilp, filter_soilp, 'bulk')
+
+    if ( use_c13 ) then
+      call CStateUpdate2(num_soilc,filter_soilc,num_soilp,filter_soilp,'c13')
+    end if
+
+    if ( use_c14 ) then
+      call CStateUpdate2(num_soilc,filter_soilc,num_soilp,filter_soilp,'c14')
+    end if
+
+    call NStateUpdate2(num_soilc, filter_soilc, num_soilp, filter_soilp)
+       
+    if (fpftdyn /= ' ') then
+      call CNHarvest(num_soilc, filter_soilc, num_soilp, filter_soilp)
+    end if 
+
+    if ( use_c13 ) then
+      call CIsoFlux2h(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c13')
+    end if
+
+    if ( use_c14 ) then
+      call CIsoFlux2h(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c14')
+    end if
+
+    call CStateUpdate2h(num_soilc,filter_soilc,num_soilp,filter_soilp,'bulk')
+
+    if ( use_c13 ) then
+      call CStateUpdate2h(num_soilc,filter_soilc,num_soilp,filter_soilp,'c13')
+    end if
+
+    if ( use_c14 ) then
+      call CStateUpdate2h(num_soilc,filter_soilc,num_soilp,filter_soilp,'c14')
+    end if
+
+    call NStateUpdate2h(num_soilc, filter_soilc, num_soilp, filter_soilp)
+       
+    call CNWoodProducts(num_soilc, filter_soilc)
+       
+    call CNFireArea(num_soilc, filter_soilc,num_soilp, filter_soilp)
+
+    call CNFireFluxes(num_soilc, filter_soilc, num_soilp, filter_soilp)
+
+    call CNNLeaching(lbc, ubc, num_soilc, filter_soilc)
+
+    if ( use_c13 ) then
+      call CIsoFlux3(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c13')
+    end if
+
+    if ( use_c14 ) then
+      call CIsoFlux3(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c14')
+    end if
+
+    call CStateUpdate3(num_soilc,filter_soilc,num_soilp,filter_soilp,'bulk')
+
+    if ( use_c13 ) then
+      call CStateUpdate3(num_soilc,filter_soilc,num_soilp,filter_soilp,'c13')
+    end if
+
+    if ( use_c14 ) then
+      call CStateUpdate3(num_soilc,filter_soilc,num_soilp,filter_soilp,'c14')
+      call C14Decay(num_soilc, filter_soilc, num_soilp, filter_soilp)
+      call C14BombSpike(num_soilp, filter_soilp)
+    end if
+
+    call NStateUpdate3(num_soilc, filter_soilc, num_soilp, filter_soilp)
+
+    call CNPrecisionControl(num_soilc, filter_soilc, num_soilp, filter_soilp)
+
+    if (doalb) then   
+      call CNVegStructUpdate(num_soilp, filter_soilp)
+    end if
+
+!   call CNAnnualUpdate(num_soilc, filter_soilc, num_soilp, filter_soilp)
+       
+    call CSummary(num_soilc, filter_soilc, num_soilp, filter_soilp, 'bulk')
+       
+    if ( use_c13 ) then
+      call CSummary(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c13')
+    end if
+
+    if ( use_c14 ) then
+      call CSummary(num_soilc, filter_soilc, num_soilp, filter_soilp, 'c14')
+    end if
+       
+    call NSummary(num_soilc, filter_soilc, num_soilp, filter_soilp)
 
   end subroutine CNEcosystemDyn
 #endif
-!-----------------------------------------------------------------------
+
 end module mod_clm_cnecosystemdyn
