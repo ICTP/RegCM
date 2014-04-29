9b727da7df577d4da906264f36aed6a839314877
Graziano Giuliani <ggiulian@ictp.it>
2014-04-29 13:22:27 +0000

    (regcm-core) : Build by default a SMOOTH topography.

--- Share/mod_dynparam.F90
+++ Share/mod_dynparam.F90
@@ -499,7 +499,7 @@ module mod_dynparam
 
     h2ohgt = .false.
     h2opct = 50.0D0
-    ismthlev = 1
+    ismthlev = 3
     rewind(ipunit)
     read(ipunit, nml=terrainparam, iostat=iresult)
     if ( iresult /= 0 ) then
