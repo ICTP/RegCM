9bc4b064b36b732e542b36f910f792cd3c8681a0
Graziano Giuliani <ggiulian@ictp.it>
2014-05-11 17:04:43 +0000

    (regcm-core) : Upgrade to netCDF

--- Tools/Programs/cumclouds/cumclouds.f90
+++ Tools/Programs/cumclouds/cumclouds.f90
@@ -1,4 +1,5 @@
 program cumclouds
+  use netcdf
   implicit none
   integer , parameter :: nk = 23     ! Num vert layers
   integer , parameter :: nds = 8     ! Num testing resolutions
@@ -8,16 +9,20 @@ program cumclouds
   real(8) , parameter :: dsinc = 25.0D0 ! Increment of ds
   real(8) , parameter :: clfrcvmax = 1.0D0 ! Maximum convective cloud fraction
 
-  real(8) , parameter :: maxcloud_dp = 45.0D0 ! In cb, cloud depth to have
+  real(8) , parameter :: maxcloud_dp = 60.0D0 ! In cb, cloud depth to have
                                               ! Maximum cloud for this grid
                                               ! resolution
 
   real(8) , parameter :: deltap = 5.0 ! Testing deltap
 
-  integer :: k , ktop , kbot , ids , kclth , ikh
+  integer :: k , ktop , kbot , ids , kclth , ikh , irec
+  integer :: istat , ncid
+  integer , dimension(2) :: idims , istart , icount
+  integer , dimension(5) :: ivars
 
   real(8) :: dp , ds , dxtemc , clfrcv , scalep , totcf , akclth
   real(8) :: scalef , totcf_old
+  real(8) , dimension(1) :: helpme
   
   real(8) :: afracl = 0.250D0
   real(8) :: afracs = clfrcvmax
@@ -31,8 +36,10 @@ program cumclouds
   real(8) , dimension(10) :: rnum
   real(8) , dimension(nk) :: rcldfra
   real(8) , dimension(nk) :: old_rcldfra
+  real(8) , dimension(nk) :: plevs
+  real(8) , dimension(nk) :: sigma
 
-  integer , parameter :: iun = 99 , gun = 100
+  integer , parameter :: gun = 100
   integer :: nseed
   character(len=256) :: fname , gname
   logical :: addnoise = .false.
@@ -48,18 +55,35 @@ program cumclouds
     call random_seed(put=iseed)
   end if
 
-  fixed_cld_profile(1)  = 0.195D0
-  fixed_cld_profile(2)  = 0.175D0
-  fixed_cld_profile(3)  = 0.155D0
-  fixed_cld_profile(4)  = 0.105D0
-  fixed_cld_profile(5)  = 0.085D0
-  fixed_cld_profile(6)  = 0.075D0
-  fixed_cld_profile(7)  = 0.065D0
-  fixed_cld_profile(8)  = 0.055D0
-  fixed_cld_profile(9)  = 0.045D0
-  fixed_cld_profile(10) = 0.045D0
+  fixed_cld_profile(1)  = 0.130D0
+  fixed_cld_profile(2)  = 0.125D0
+  fixed_cld_profile(3)  = 0.120D0
+  fixed_cld_profile(4)  = 0.080D0
+  fixed_cld_profile(5)  = 0.080D0
+  fixed_cld_profile(6)  = 0.080D0
+  fixed_cld_profile(7)  = 0.085D0
+  fixed_cld_profile(8)  = 0.085D0
+  fixed_cld_profile(9)  = 0.105D0
+  fixed_cld_profile(10) = 0.110D0
+
+  plevs(nk) = 963.0D0
+  call getsigma(sigma)
+  do k = 1 , nk
+    plevs(k) = sigma(k)*plevs(nk) + 50.0D0
+  end do
 
   do ids = 0 , nds
+
+    write(fname,'(a,i0.4,a)') 'profile_', int(ds), '.nc'
+    istat = nf90_create(fname,NF90_CLOBBER,ncid)
+    istat = nf90_def_dim(ncid,'klev',nk,idims(1))
+    istat = nf90_def_dim(ncid,'record',nf90_unlimited,idims(2))
+    istat = nf90_def_var(ncid,'klev',nf90_float,idims(1:1),ivars(1))
+    istat = nf90_def_var(ncid,'cld',nf90_float,idims,ivars(2))
+    istat = nf90_def_var(ncid,'totcf',nf90_float,idims(2:2),ivars(3))
+    istat = nf90_def_var(ncid,'oldcld',nf90_float,idims,ivars(4))
+    istat = nf90_def_var(ncid,'oldtotcf',nf90_float,idims(2:2),ivars(5))
+    istat = nf90_put_att(ncid,nf90_global,'ds',ds)
 
     ! This part is in mod_param :
     !  For a given grid size , fix a Maximum Convective Cloud Cover
@@ -68,23 +92,22 @@ program cumclouds
     else
       ds = dsinc*dble(ids)
     end if
-    write(fname,'(a,i0.4,a)') 'profile_', int(ds), '.txt'
     write(gname,'(a,i0.4,a)') 'gnuplot_', int(ds), '.dat'
-    open(file=fname,form='formatted',unit=iun)
     open(file=gname,form='formatted',unit=gun)
     dxtemc = dmin1(dmax1(ds,dsmalc),dlargc)
     clfrcv = afracl + (afracs-afracl)*((dlargc-dxtemc)/(dlargc-dsmalc))**2
     clfrcv = dmin1(clfrcv,1.0D0)
-    write(iun,'(a,f11.6)') 'Horizontal Resolution ds = ',ds
-    write(iun,*) 'Convective Cloud Cover parameters after resolution scaling'
-    write(iun,'(a,f11.6)') '  Maximum Convective Cloud Cover : ',clfrcv
+    istat = nf90_put_att(ncid,nf90_global,'clfrcv',clfrcv)
+    istat = nf90_enddef(ncid)
 
+    istat = nf90_put_var(ncid,idims(1),plevs)
     ! Test the cloud generation.
     ! Input are kbot and ktop (maximum cloud top and bottom)
     ! Have them assume all possible ranges.
 
-    do ktop = maxtopcloud , nk-3
-      do kbot = ktop+1 , nk-2
+    irec = 1
+    do kbot = nk-2 , 4 , -1
+      do ktop = kbot-1 , 3 , -1
 
         rcldfra(:) = 0.0D0
         old_rcldfra(:) = 0.0D0
@@ -98,7 +121,7 @@ program cumclouds
         end if
 
         kclth = kbot-ktop
-        dp = dble(kclth)*deltap
+        dp = plevs(kbot) - plevs(ktop)
         scalep = min(dp/maxcloud_dp,1.0D0)
         akclth = 1.0D0/dble(kclth)
 
@@ -116,22 +139,103 @@ program cumclouds
           old_rcldfra(k) = 1.0D0 - scalef**akclth
         end do
 
+        istart(1) = 1
+        istart(2) = irec
+        icount(1) = nk
+        icount(2) = 1
+        istat = nf90_put_var(ncid,ivars(2),rcldfra,istart,icount)
+        istat = nf90_put_var(ncid,ivars(4),old_rcldfra,istart,icount)
         ! Compute the cloud fraction as seen by the radiative model
         totcf = 1.0D0
         totcf_old = 1.0D0
         do k = 1 , nk
-          write(iun,'(i3,2f11.6)') k , rcldfra(k) , old_rcldfra(k)
           totcf = totcf * ( 1.0D0 - rcldfra(k) )
           totcf_old = totcf_old * ( 1.0D0 - old_rcldfra(k) )
         end do
         totcf = 1.0D0 - totcf
-        write(iun,'(a,3f11.6)') 'Total cloud fraction ', dp , totcf , totcf_old
         write(gun,'(i4,3f11.6)') kclth , dp , totcf , totcf_old
-
+        istart(1) = irec
+        icount(1) = 1
+        helpme(1) = totcf
+        istat = nf90_put_var(ncid,ivars(3),helpme,istart,icount)
+        helpme(1) = totcf_old
+        istat = nf90_put_var(ncid,ivars(5),helpme,istart,icount)
+        irec = irec + 1
       end do
     end do
-    close(iun)
+    istat = nf90_close(ncid)
     close(gun)
   end do
+
+  contains
+
+    subroutine getsigma(sigma)
+      implicit none
+      real(8) , intent(out) , dimension(:) :: sigma
+      integer :: kz
+      kz = size(sigma)
+      if ( kz == 14 ) then                      ! RegCM2
+        sigma(1) = 0.0D0
+        sigma(2) = 0.04D0
+        sigma(3) = 0.10D0
+        sigma(4) = 0.17D0
+        sigma(5) = 0.25D0
+        sigma(6) = 0.35D0
+        sigma(7) = 0.46D0
+        sigma(8) = 0.56D0
+        sigma(9) = 0.67D0
+        sigma(10) = 0.77D0
+        sigma(11) = 0.86D0
+        sigma(12) = 0.93D0
+        sigma(13) = 0.97D0
+        sigma(14) = 0.99D0
+        sigma(15) = 1.0D0
+      else if ( kz == 18 ) then                 ! RegCM3, default
+        sigma(1) = 0.0D0
+        sigma(2) = 0.05D0
+        sigma(3) = 0.10D0
+        sigma(4) = 0.16D0
+        sigma(5) = 0.23D0
+        sigma(6) = 0.31D0
+        sigma(7) = 0.39D0
+        sigma(8) = 0.47D0
+        sigma(9) = 0.55D0
+        sigma(10) = 0.63D0
+        sigma(11) = 0.71D0
+        sigma(12) = 0.78D0
+        sigma(13) = 0.84D0
+        sigma(14) = 0.89D0
+        sigma(15) = 0.93D0
+        sigma(16) = 0.96D0
+        sigma(17) = 0.98D0
+        sigma(18) = 0.99D0
+        sigma(19) = 1.0D0
+      else if ( kz == 23 ) then                 ! MM5V3
+        sigma(1) = 0.0D0
+        sigma(2) = 0.05D0
+        sigma(3) = 0.1D0
+        sigma(4) = 0.15D0
+        sigma(5) = 0.2D0
+        sigma(6) = 0.25D0
+        sigma(7) = 0.3D0
+        sigma(8) = 0.35D0
+        sigma(9) = 0.4D0
+        sigma(10) = 0.45D0
+        sigma(11) = 0.5D0
+        sigma(12) = 0.55D0
+        sigma(13) = 0.6D0
+        sigma(14) = 0.65D0
+        sigma(15) = 0.7D0
+        sigma(16) = 0.75D0
+        sigma(17) = 0.8D0
+        sigma(18) = 0.85D0
+        sigma(19) = 0.89D0
+        sigma(20) = 0.93D0
+        sigma(21) = 0.96D0
+        sigma(22) = 0.98D0
+        sigma(23) = 0.99D0
+        sigma(24) = 1.0D0
+      end if
+    end subroutine getsigma
 
 end program cumclouds
--- Tools/Programs/cumclouds/plotclf.gp
+++ Tools/Programs/cumclouds/plotclf.gp
@@ -5,13 +5,13 @@ set output 'Cloud_fraction.ps'
 set xlabel 'Depth of cloud [mb]'
 set ylabel 'Total cloud fraction [%]'
 set title  'RegCM cumulus cloud model'
-plot 'dat.dat' u ($2*10):($3*100)   title '10 km grid', \
-     'dat.dat' u ($6*10):($7*100)   title '25 km grid', \
-     'dat.dat' u ($10*10):($11*100)   title '50 km grid', \
-     'dat.dat' u ($14*10):($15*100) title '75 km grid', \
-     'dat.dat' u ($18*10):($19*100) title '100km grid', \
-     'dat.dat' u ($22*10):($23*100) title '125km grid', \
-     'dat.dat' u ($26*10):($27*100) title '150km grid', \
-     'dat.dat' u ($30*10):($31*100) title '175km grid', \
-     'dat.dat' u ($34*10):($35*100) title '200km grid', \
-     'dat.dat' u ($10*10):($12*100) title 'old 50 km grid'
+plot 'dat.dat' u ($2):($3*100)   title '10 km grid', \
+     'dat.dat' u ($6):($7*100)   title '25 km grid', \
+     'dat.dat' u ($10):($11*100)   title '50 km grid', \
+     'dat.dat' u ($14):($15*100) title '75 km grid', \
+     'dat.dat' u ($18):($19*100) title '100km grid', \
+     'dat.dat' u ($22):($23*100) title '125km grid', \
+     'dat.dat' u ($26):($27*100) title '150km grid', \
+     'dat.dat' u ($30):($31*100) title '175km grid', \
+     'dat.dat' u ($34):($35*100) title '200km grid', \
+     'dat.dat' u ($10):($12*100) title 'old 50 km grid'
