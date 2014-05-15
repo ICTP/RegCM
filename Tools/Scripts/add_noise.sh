65247dc30f2e3c900ea53cf6771f03633a390464
Graziano Giuliani <ggiulian@ictp.it>
2014-05-15 07:38:53 +0000

    (regcm-core) : EXPLICIT set RNG seed for the GSL. The default seed may generate SAME series. Advise user to use perturbparam.

--- Tools/Scripts/add_noise.sh
+++ Tools/Scripts/add_noise.sh
@@ -2,10 +2,12 @@
 
 # This can be used to add a random noise to an ICBC file
 # The range here is -0.25,+0.25
+# This method is deprecated. Use the perturbparam in the namelist!
 
 export GSL_RNG_TYPE=mt19937
+export GSL_RNG_SEED=`date +%s`
 for file in $@
 do
-  ncap2 -O -s 't=t+0.5*(0.5-gsl_rng_uniform(t))' \
+  ncap2 -O -s 't=t+0.5*(0.5-gsl_rng_uniform_pos(t))' \
           $file `basename $file.nc`_perturb.nc
 done
