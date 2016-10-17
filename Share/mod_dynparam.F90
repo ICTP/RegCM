0b4d8fb5488a8056b4f3b944d3d3bf7f578b5875
Graziano Giuliani <ggiulian@ictp.it>
2016-10-17 14:21:54 +0000

    (regcm-core) : revert boundary modifications for terrain

--- Share/mod_dynparam.F90
+++ Share/mod_dynparam.F90
@@ -539,7 +539,7 @@ module mod_dynparam
     moist_filename = 'moist.nc'
     tersrc = 'GMTED'
 
-    smthbdy = .true.
+    smthbdy = .false.
     h2ohgt = .true.
     h2opct = 50.0_rkx
     ismthlev = 1
