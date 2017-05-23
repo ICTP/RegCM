958fa017e6d4b39cfb4f31cc20b5aa976faa344a
Graziano Giuliani <ggiulian@ictp.it>
2017-05-23 08:15:56 +0000

    (regcm-core) : review script.

--- Tools/Scripts/prepare_cfs_for_regcm.sh
+++ Tools/Scripts/prepare_cfs_for_regcm.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-WORKDIR=/home/graziano/test/RCMDATA/CFS/GRIB
-CFSDIR=/home/graziano/test/RCMDATA/CFS
-
-PROTO=http
-SITE=nomads.ncep.noaa.gov
-SOURCE=pub/data/nccf/com/cfs/prod/cfs
+WORKDIR=/scratch/ggiulian/test/cfsdata/GRIB
+CFSDIR=/scratch/ggiulian/test/cfsdata/CFS
 
 run=00     # The run (one in 00-06-12-18)
 emem=01    # The ensemble member (one in 01 02 03 04)
 
+download=1 # To also download the data
 do_pack=1  # Keep this to lower disk usage
 
 DWN=`which wget`
@@ -25,23 +22,54 @@ then
   exit 1
 fi
 
+mkdir -p $WORKDIR
 cd $WORKDIR
 
-today=`date +%Y%m%d`
+today=`date -u +%Y%m%d`
+year=`date -u +%Y`
+month=`date -u +%m`
+day=`date -u +%d`
 
 # TO TEST FOR A PARTICULAR DATE
 
-today=20130416
-
-DSITE=${PROTO}://${SITE}/${SOURCE}/cfs.${today}/${run}/6hrly_grib_${emem}
+#today=20170522
+#year=2017
+#month=05
+#day=22
 
 cmdline="-nc_table prc.table -nc4 -netcdf"
-ocnf=ocnf
 pgbf=pgbf
-
-echo $DSITE
+pgbanl=pgbanl
+flxf=flxf
 
 # Download data
+if [ $download -eq 1 ]
+then
+  PROTO=http
+  SITE=nomads.ncep.noaa.gov
+  SOURCE=pub/data/nccf/com/cfs/prod/cfs
+  DSITE=${PROTO}://${SITE}/${SOURCE}/cfs.${today}/${run}/6hrly_grib_${emem}
+  echo $DSITE
+  for (( d = 0; d < 32; d ++ ))
+  do
+    for (( h = 0; h < 24; h += 6 ))
+    do
+      dd=$(( $d * 24 + $h ))
+      d1=`date -u +%Y%m%d%H \
+	      --date "$year-$month-$day $run:00:00 UTC + $dd hours"`
+      if [ $d -eq 0 -a $h -eq 0 ]
+      then
+        wget -c $DSITE/${pgbanl}.${emem}.$year$month$day$run.grb2
+        wget -c $DSITE/${flxf}${d1}.${emem}.${today}${run}.grb2
+        continue
+      fi
+      for file in ${flxf} ${pgbf}
+      do
+        wget -c $DSITE/${file}${d1}.${emem}.${today}${run}.grb2
+      done
+    done
+  done
+fi
 
 # Change data format
 
@@ -69,8 +97,10 @@ fi
 echo '#EOF'                                            >> prc.table
 
 # Prepare
-cat ${ocnf}*.grb2 > ocn.grb2
-cat ${pgbf}*.grb2 > plv.grb2
+cat ${flxf}*.grb2 > flx.grb2
+cat ${pgbanl}.${emem}.$year$month$day$run.grb2 ${pgbf}*.grb2 > plv.grb2
+cat ${flxf}${today}${run}.${emem}.${today}${run}.grb2 >> \
+	${pgbanl}.${emem}.$year$month$day$run.grb2
 
 START_TIME=${today}${run}
 PLVDEST=${CFSDIR}/${START_TIME}/${emem}/PLEV
@@ -80,7 +110,7 @@ mkdir -p $SSTDEST
 
 # Process
 $WG2 -match 'TMP:surface'        \
-  $cmdline ${SSTDEST}/sst.${START_TIME}.nc  ocn.grb2
+  $cmdline ${SSTDEST}/sst.${START_TIME}.nc  flx.grb2
 $WG2 -match ':TMP' -match 'mb:'  \
   $cmdline ${PLVDEST}/air.${START_TIME}.nc  plv.grb2
 $WG2 -match ':RH' -match 'mb:'   \
@@ -93,5 +123,5 @@ $WG2 -match ':HGT' -match 'mb:'  \
   $cmdline ${PLVDEST}/hgt.${START_TIME}.nc  plv.grb2
 
 # Cleanup
-rm -f ocn.grb2 plv.grb2 prc.table
-# rm -f ${ocnf}*.grb2 ${pgbf}*.grb2
+rm -f flx.grb2 plv.grb2 prc.table
+rm -f ${flxf}*.grb2 ${pgbf}*.grb2
