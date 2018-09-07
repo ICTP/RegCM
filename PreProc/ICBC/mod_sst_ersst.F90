1dda021d97f2db2151ff3475956e100c5257c002
Graziano Giuliani <ggiulian@ictp.it>
2018-09-07 10:02:23 +0000

    (regcm-core) : Update to use ERSST v5

--- PreProc/ICBC/mod_sst_ersst.F90
+++ PreProc/ICBC/mod_sst_ersst.F90
@@ -35,13 +35,15 @@ module mod_sst_ersst
   public :: sst_ersst
 
   integer(ik4) :: ilon , jlat
-  integer(ik4) , parameter :: idtbc = 6
   real(rkx) , pointer , dimension(:,:) :: sst
   real(rkx) , pointer , dimension(:) :: lati
   real(rkx) , pointer , dimension(:) :: loni
-  integer(2) , pointer , dimension(:,:) :: work
+  integer(2) , pointer , dimension(:,:,:) :: work
+  real(rkx) , dimension(2) :: xadd , xscale , xmiss
 
   integer(ik4) :: inet
+
+  character(len=256) :: inpfile
 
   type(h_interpolator) :: hint
 
@@ -49,58 +51,51 @@ module mod_sst_ersst
   !
   ! Comments on dataset sources and location:
   !
-  ! ERAIN    ERAIN_SST is provided by ERA-Interim project
-  !          6 hourly frequncy, 1.5x1.5 degree resolution
-  !          from 1989010100 to present.
-  !          'ERSST' for using the sea surface temperature;
-  !          'ERSKT' for using the skin temperature.
-  !
-  ! ML = 1 is   0.0; ML = 2 is   1.5; => ML = 240 is 358.5E
-  ! NL = 1 is  90.0; ML = 2 is  88.5; => ML = 121 is -90.
+  ! ERSST    Extended Reconstructed Sea Surface Temperature (ERSST)
+  !          Monthly from January 1854 to the present
   !
   subroutine sst_ersst
     implicit none
     integer(ik4) :: it
     integer(ik4) :: istatus
-    integer(ik4) :: year , month , day , hour , isyear = -1
+    integer(ik4) :: year , month , day , hour
     integer(ik4) :: dimi , vari
-    integer(ik4) :: ierrec , nsteps
-    type(rcm_time_and_date) :: idate , ierastart
-    type(rcm_time_interval) :: tdiff , itbc
-    character(len=256) :: inpfile
+    integer(ik4) :: nsteps
+    type(rcm_time_and_date) :: idate , idatef , idateo
     logical :: lfirst
 
     data lfirst /.true./
 
-    itbc = rcm_time_interval(idtbc,uhrs)
-    tdiff = globidate2-globidate1
-    nsteps = nint(tohours(tdiff))/idtbc + 1
+    idateo = monfirst(globidate1)
+    idatef = monfirst(globidate2)
+    if (idatef < globidate2) then
+      idatef = nextmon(idatef)
+    end if
+    nsteps = imondiff(idatef,idateo) + 1
 
     write (stdout,*) 'GLOBIDATE1 : ' , tochar(globidate1)
     write (stdout,*) 'GLOBIDATE2 : ' , tochar(globidate2)
     write (stdout,*) 'NSTEPS     : ' , nsteps
 
-    call open_sstfile(globidate1)
-
     ! SET UP LONGITUDES AND LATITUDES FOR SST DATA
 
-    call split_idate(globidate1,year,month,day,hour)
-    write (inpfile,'(a,i0.4,a)') &
-      trim(inpglob)//pthsep//ssttyp//pthsep//'SST'//pthsep//'sst.',year, '.nc'
+    idate = prevmon(globidate1)
+    call split_idate(idate,year,month,day,hour)
+    write (inpfile,'(a,i0.4,i0.2,a)') &
+      trim(inpglob)//pthsep//'SST'//pthsep//ssttyp// &
+      pthsep//'ersst.v5.',year, month, '.nc'
     istatus = nf90_open(inpfile,nf90_nowrite,inet)
     call checkncerr(istatus,__FILE__,__LINE__, &
             'Cannot open file '//trim(inpfile))
-    isyear = year
-    ierastart = year*1000000 + 10100
     lfirst = .true.
     write (stdout,*) 'Opened ', trim(inpfile)
-    istatus = nf90_inq_dimid(inet,'longitude',dimi)
+    istatus = nf90_inq_dimid(inet,'lon',dimi)
     call checkncerr(istatus,__FILE__,__LINE__, &
                     'Error find lon dim')
     istatus = nf90_inquire_dimension(inet,dimi,len=ilon)
     call checkncerr(istatus,__FILE__,__LINE__, &
                     'Error inquire lon dim')
-    istatus = nf90_inq_dimid(inet,'latitude',dimi)
+    istatus = nf90_inq_dimid(inet,'lat',dimi)
     call checkncerr(istatus,__FILE__,__LINE__, &
                     'Error find lat dim')
     istatus = nf90_inquire_dimension(inet,dimi,len=jlat)
@@ -110,15 +105,15 @@ module mod_sst_ersst
     call getmem1d(loni,1,ilon,'sst_ersst:loni')
     call getmem1d(lati,1,jlat,'sst_ersst:lati')
     call getmem2d(sst,1,ilon,1,jlat,'sst_ersst:sst')
-    call getmem2d(work,1,ilon,1,jlat,'sst_ersst:work')
+    call getmem3d(work,1,ilon,1,jlat,1,2,'sst_ersst:work')
 
-    istatus = nf90_inq_varid(inet,'longitude',vari)
+    istatus = nf90_inq_varid(inet,'lon',vari)
     call checkncerr(istatus,__FILE__,__LINE__, &
                     'Error find lon var')
     istatus = nf90_get_var(inet,vari,loni)
     call checkncerr(istatus,__FILE__,__LINE__, &
                     'Error read lon var')
-    istatus = nf90_inq_varid(inet,'latitude',vari)
+    istatus = nf90_inq_varid(inet,'lat',vari)
     call checkncerr(istatus,__FILE__,__LINE__, &
                     'Error find lat var')
     istatus = nf90_get_var(inet,vari,lati)
@@ -127,101 +122,101 @@ module mod_sst_ersst
 
     call h_interpolator_create(hint,lati,loni,xlat,xlon)
 
-    idate = globidate1
+    call open_sstfile(idateo)
+
+    idate = idateo
     do it = 1 , nsteps
-      call split_idate(idate,year,month,day,hour)
-      if ( year /= isyear ) then
-        istatus = nf90_close(inet)
-        call checkncerr(istatus,__FILE__,__LINE__, &
-                        'Cannot close file')
-        write (inpfile,'(a,i0.4,a)') &
-               trim(inpglob)//pthsep//ssttyp//pthsep// &
-               'SST'//pthsep//'sst.',year, '.nc'
-        istatus = nf90_open(inpfile,nf90_nowrite,inet)
-        call checkncerr(istatus,__FILE__,__LINE__, &
-                        'Cannot open file '//trim(inpfile))
-        write (stdout,*) 'Opened ', trim(inpfile)
-        isyear = year
-        ierastart = year*1000000 + 10100
-        lfirst = .true.
-      end if
-
-      tdiff = idate-ierastart
-      ierrec = nint(tohours(tdiff))/idtbc+1
-
-      if ( ssttyp == 'ERSKT' ) then
-        call sst_erain(ierrec,lfirst,2)
-      else
-        call sst_erain(ierrec,lfirst,1)
-      end if
-
+      call sst_readersst(idate,lfirst)
       call h_interpolate_cont(hint,sst,sstmm)
-
       call writerec(idate)
       write(stdout,*) 'WRITING OUT SST DATA:' , tochar(idate)
-      idate = idate + itbc
+      idate = nextmon(idate)
     end do
 
     call h_interpolator_destroy(hint)
 
   end subroutine sst_ersst
 
-  subroutine sst_erain(it,lfirst,itype)
+  subroutine sst_readersst(idate,lfirst)
     use netcdf
     implicit none
-    integer(ik4) , intent(in) :: it , itype
+    type(rcm_time_and_date) , intent(in) :: idate
     logical , intent(inout) :: lfirst
     integer(ik4) :: i , j
-    character(len=4) , dimension(2) :: varname
-    integer(ik4) :: istatus
-    integer(ik4) , save :: ivar
-    real(rkx) , save :: xadd , xscale , xmiss
-    integer(ik4) , dimension(3) , save :: icount , istart
+    integer(ik4) :: year , month , day , hour
+    integer(ik4) :: istatus , ivar
     !
     ! The data are packed into short integers (INTEGER*2).  The array
     ! work will be used to hold the packed integers. The array 'sst'
     ! will contain the unpacked data.
     !
-    data varname/'sst','skt'/
     if ( lfirst ) then
-      istatus = nf90_inq_varid(inet,varname(itype),ivar)
+      istatus = nf90_inq_varid(inet,'sst',ivar)
       call checkncerr(istatus,__FILE__,__LINE__, &
-                      'Cannot find variable '//trim(varname(itype)))
-      istatus = nf90_get_att(inet,ivar,'scale_factor',xscale)
+                      'Cannot find variable sst')
+      istatus = nf90_get_att(inet,ivar,'scale_factor',xscale(1))
       call checkncerr(istatus,__FILE__,__LINE__, &
                       'Cannot get attribute scale_factor')
-      istatus = nf90_get_att(inet,ivar,'add_offset',xadd)
+      istatus = nf90_get_att(inet,ivar,'add_offset',xadd(1))
       call checkncerr(istatus,__FILE__,__LINE__, &
                       'Cannot get attribute add_offset')
-      istatus = nf90_get_att(inet,ivar,'_FillValue',xmiss)
+      istatus = nf90_get_att(inet,ivar,'_FillValue',xmiss(1))
       call checkncerr(istatus,__FILE__,__LINE__, &
                       'Cannot get attribute _FillValue')
-      istart(1) = 1
-      istart(2) = 1
-      icount(1) = ilon
-      icount(2) = jlat
+      istatus = nf90_get_var(inet,ivar,work(:,:,1))
+      call checkncerr(istatus,__FILE__,__LINE__, &
+                     'Cannot read sst')
+      istatus = nf90_close(inet)
+      call checkncerr(istatus,__FILE__,__LINE__, &
+                      'Cannot close file')
       lfirst = .false.
+    else
+      xscale(1) = xscale(2)
+      xadd(1) = xadd(2)
+      xmiss(1) = xmiss(2)
+      work(:,:,1) = work(:,:,2)
     end if
 
-    istart(3) = it
-    icount(3) = 1
-    istatus = nf90_get_var(inet,ivar,work,istart,icount)
+    call split_idate(idate,year,month,day,hour)
+    write (inpfile,'(a,i0.4,i0.2,a)') &
+      trim(inpglob)//pthsep//'SST'//pthsep//ssttyp// &
+      pthsep//'ersst.v5.',year, month, '.nc'
+    istatus = nf90_open(inpfile,nf90_nowrite,inet)
     call checkncerr(istatus,__FILE__,__LINE__, &
-                   'Cannot read '//trim(varname(itype)))
-
+                    'Cannot open file '//trim(inpfile))
+    write (stdout,*) 'Opened ', trim(inpfile)
+    istatus = nf90_inq_varid(inet,'sst',ivar)
+    call checkncerr(istatus,__FILE__,__LINE__, &
+                    'Cannot find variable sst')
+    istatus = nf90_get_att(inet,ivar,'scale_factor',xscale(2))
+    call checkncerr(istatus,__FILE__,__LINE__, &
+                    'Cannot get attribute scale_factor')
+    istatus = nf90_get_att(inet,ivar,'add_offset',xadd(2))
+    call checkncerr(istatus,__FILE__,__LINE__, &
+                    'Cannot get attribute add_offset')
+    istatus = nf90_get_att(inet,ivar,'_FillValue',xmiss(2))
+    call checkncerr(istatus,__FILE__,__LINE__, &
+                      'Cannot get attribute _FillValue')
+    istatus = nf90_get_var(inet,ivar,work(:,:,2))
+    call checkncerr(istatus,__FILE__,__LINE__, &
+                   'Cannot read sst')
+    istatus = nf90_close(inet)
+    call checkncerr(istatus,__FILE__,__LINE__, &
+                    'Cannot close file')
     do j = 1 , jlat
       do i = 1 , ilon
-        if (work(i,j) /= xmiss) then
-          sst(i,j) = real(work(i,j),rkx)*xscale + xadd
+        if ( work(i,j,1) /= xmiss(1) .and. work(i,j,2) /= xmiss(2) ) then
+          sst(i,j) = 0.5_rkx * &
+                ( real(work(i,j,1),rkx)*xscale(1) + xadd(1) + 273.15_rkx + &
+                  real(work(i,j,2),rkx)*xscale(2) + xadd(2) + 273.15_rkx )
           ! Respect convention for ice
-          if ( sst(i,j) < 271.46_rkx ) sst(i,j) = 271.46_rkx
+          if ( sst(i,j) < 271.34_rkx ) sst(i,j) = 271.46_rkx
         else
           sst(i,j) = -9999.0_rkx
         end if
-        ! Respect convention on seaice
       end do
     end do
-  end subroutine sst_erain
+  end subroutine sst_readersst
 
 end module mod_sst_ersst
 ! vim: tabstop=8 expandtab shiftwidth=2 softtabstop=2
