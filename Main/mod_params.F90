b968dc1623e7297bf34acb69467fc4a13f9e314e
Graziano Giuliani <ggiulian@ictp.it>
2020-07-20 16:42:42 +0200

    change default for NH run

--- Main/mod_params.F90
+++ Main/mod_params.F90
@@ -286,7 +286,7 @@ module mod_params
     !
     ! Non hydrostatic param ;
     !
-    ifupr = 1
+    ifupr = 0
     nhbet = 0.4_rkx   ! Arakawa beta (MM5 manual, Sec. 2.5.1)
     nhxkd = 0.1_rkx
     itopnudge = 0
