709c6f01fee896b15fe015c858c90b64e4f1d641
Graziano Giuliani <ggiulian@ictp.it>
2020-11-23 19:06:37 +0100

    sorry...

--- PreProc/ICBC/mod_grid.F90
+++ PreProc/ICBC/mod_grid.F90
@@ -129,6 +129,10 @@ module mod_grid
                        mask=mask,lndcat=landuse,msfx=msfx,msfd=msfd)
       call read_reference_surface_temp(incin,ts0)
       call nhsetup(ptop,base_state_pressure,logp_lrate,ts0)
+      do k = 1 , kz
+        sigmah(k) = d_half*(sigmaf(k+1)+sigmaf(k))
+        dsigma(k) = (sigmaf(k+1)-sigmaf(k))
+      end do
     else if ( idynamic == 3 ) then
       call read_domain(incin,sigmaf,xlat,xlon,ulat=ulat,ulon=ulon, &
                        vlat=vlat,vlon=vlon,ht=topogm,mask=mask,    &
