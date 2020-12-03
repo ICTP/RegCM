5b53e88506d96b895669fbf6dd0167cdff615826
Graziano Giuliani <ggiulian@ictp.it>
2020-12-03 18:20:18 +0100

    document it

--- Main/mod_bmiregcm.F90
+++ Main/mod_bmiregcm.F90
@@ -18,6 +18,28 @@
 !::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 #ifdef DEVELOPMENT
+!
+! This is for now just a tentative idea.
+!
+! https://github.com/csdms/bmi
+!
+! The Basic Model Interface (BMI) is a standardized set of functions that allows
+! coupling of models to models and models to data.
+! The Basic Model Interface (BMI) is a library specification created by the
+! Community Surface Dynamics Modeling System (CSDMS) to facilitate the
+! conversion of a model or dataset into a reusable, plug-and-play component.
+! Recall that, in this context, an interface is a named set of functions with
+! prescribed arguments and return values.
+! The BMI functions make a model self-describing and fully controllable by a
+! modeling framework or application. By design, the BMI functions are
+! straightforward to implement in any language, using only simple
+! data types from standard language libraries. Also by design, the BMI functions
+! are noninvasive. This means that a model's BMI does not make calls to other
+! components or tools and is not modified to use any framework-specific data
+! structures. A BMI, therefore, introduces no dependencies into a model, so the
+! model can still be used in a stand-alone manner.
+!
+! The BMI is expressed in the Scientific Interface Definition Language (SIDL)
 
 module bmiregcm
 
