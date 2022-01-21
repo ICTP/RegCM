ce11629ec32deb6b42fd8d09ea16ee2e9b137961
Graziano Giuliani <ggiulian@ictp.it>
2022-01-21 16:28:29 +0100

    Complete CESM2

--- PreProc/ICBC/mod_cmip6.F90
+++ PreProc/ICBC/mod_cmip6.F90
@@ -52,6 +52,7 @@ module mod_cmip6
   character(len=*) , parameter , public :: cnrm_version1 = 'v20181206'
   character(len=*) , parameter , public :: cesm_version = 'v20190514'
   character(len=*) , parameter , public :: cesm_version1 = 'v20200528'
+  character(len=*) , parameter , public :: cesm_version2 = 'v20200210'
 
   public :: init_cmip6 , get_cmip6 , conclude_cmip6
 
@@ -333,15 +334,14 @@ module mod_cmip6
           ua%hcoord => orog%hcoord
           va%hcoord => orog%hcoord
           qa%hcoord => orog%hcoord
-          stop
-          !call read_2d_cesm(idate,ps,only_coord)
-          !call read_3d_cesm(idate,ta,only_coord)
+          call read_2d_cesm(idate,ps,only_coord)
+          call read_3d_cesm(idate,ta,only_coord)
           ua%vcoord => ta%vcoord
           va%vcoord => ta%vcoord
           qa%vcoord => qa%vcoord
-          !call read_3d_cesm(idate,ua,only_coord)
-          !call read_3d_cesm(idate,va,only_coord)
-          !call read_3d_cesm(idate,qa,only_coord)
+          call read_3d_cesm(idate,ua,only_coord)
+          call read_3d_cesm(idate,va,only_coord)
+          call read_3d_cesm(idate,qa,only_coord)
           nkin = nipl
           call getmem1d(sigmar,1,nkin,'cmip6:cesm:sigmar')
           do k = 1 , nkin
@@ -397,7 +397,6 @@ module mod_cmip6
           qa%hcoord => orog%hcoord
           call read_2d_normm(idate,ps,only_coord)
           call read_3d_hadmm(idate,ta,only_coord)
-          stop
           ua%vcoord => ta%vcoord
           va%vcoord => ta%vcoord
           qa%vcoord => qa%vcoord
@@ -555,7 +554,42 @@ module mod_cmip6
           call height(zvar,zp_in,ta%var,ps%var,pa_in,orog%var, &
                       ta%ni,ta%nj,ta%nk,fplev,nkin)
         case ( 'CESM2' )
-          stop
+!$OMP SECTIONS
+!$OMP SECTION
+          call read_2d_cesm(idate,ps)
+!$OMP SECTION
+          call read_3d_cesm(idate,ua)
+!$OMP SECTION
+          call read_3d_cesm(idate,va)
+!$OMP SECTION
+          call read_3d_cesm(idate,ta)
+!$OMP SECTION
+          call read_3d_cesm(idate,qa)
+!$OMP END SECTIONS
+          call sph2mxr(qa%var,qa%ni,qa%nj,qa%nk)
+          do k = 1 , ta%nk
+            do j = 1 , ta%nj
+              do i = 1 , ta%ni
+                pa_in(i,j,k) = ta%vcoord%ak(k) * ta%vcoord%p0 + &
+                       ta%vcoord%bk(k) * ps%var(i,j)
+              end do
+            end do
+          end do
+          pa_in = pa_in * 0.01_rkx
+!$OMP SECTIONS
+!$OMP SECTION
+          call intlin(uvar,ua%var,pa_in,ua%ni,ua%nj,ua%nk,fplev,nkin)
+!$OMP SECTION
+          call intlin(vvar,va%var,pa_in,va%ni,va%nj,va%nk,fplev,nkin)
+!$OMP SECTION
+          call intlog(tvar,ta%var,pa_in,ta%ni,ta%nj,ta%nk,fplev,nkin)
+!$OMP SECTION
+          call intlin(qvar,qa%var,pa_in,qa%ni,qa%nj,qa%nk,fplev,nkin)
+!$OMP END SECTIONS
+          ps%var = ps%var * 0.01_rkx
+          call htsig(zp_in,ta%var,pa_in,qa%var,ps%var,orog%var)
+          call height(zvar,zp_in,ta%var,ps%var,pa_in,orog%var, &
+                      ta%ni,ta%nj,ta%nk,fplev,nkin)
         case default
           call die(__FILE__,'__LINE__ : Unsupported cmip6 model.',-1)
       end select
@@ -1914,38 +1948,6 @@ module mod_cmip6
           'Error close file '//trim(v%filename)//'.')
     end subroutine read_fx_cnrm
 
-    recursive subroutine read_fx_cesm(v)
-      implicit none
-      type(cmip6_2d_var) , pointer , intent(inout) :: v
-      integer(ik4) :: istatus
-
-      v%filename = trim(cmip6_fxpath(cesm_version,v%vname))
-#ifdef DEBUG
-      write(stderr,*) 'Opening ',trim(v%filename)
-#endif
-      istatus = nf90_open(v%filename,nf90_nowrite,v%ncid)
-      call cmip6_error(istatus,__FILE__,__LINE__, &
-          'Error opening file '//trim(v%filename)//'.')
-      allocate(v%hcoord)
-      call read_hcoord_hadmm(v%ncid,v%hcoord%lon1d,v%hcoord%lat1d)
-      v%ni = size(v%hcoord%lon1d)
-      v%nj = size(v%hcoord%lat1d)
-      call getmem2d(v%var,1,v%ni,1,v%nj,'cmip6:cesm:'//trim(v%vname))
-#ifdef DEBUG
-      write(stderr,*) 'Input shape for ',trim(v%vname),' = ',v%ni,'x',v%nj
-#endif
-      istatus = nf90_inq_varid(v%ncid,v%vname,v%ivar)
-      call cmip6_error(istatus,__FILE__,__LINE__, &
-        'Error searchong '//trim(v%vname)//' var in file '// &
-        trim(v%filename)//'.')
-      istatus = nf90_get_var(v%ncid,v%ivar,v%var)
-      call cmip6_error(istatus,__FILE__,__LINE__, &
-          'Error read variable '//v%vname//' from '//trim(v%filename)//'.')
-      istatus = nf90_close(v%ncid)
-      call cmip6_error(istatus,__FILE__,__LINE__, &
-          'Error close file '//trim(v%filename)//'.')
-    end subroutine read_fx_cesm
-
     subroutine read_hcoord_cesm(ncid,lon,lat)
       implicit none
       integer(ik4) , intent(in) :: ncid
@@ -1998,6 +2000,286 @@ module mod_cmip6
       istatus = nf90_get_var(ncid,ivarid,p0)
       call cmip6_error(istatus,__FILE__,__LINE__,'Error read p0 var')
     end subroutine read_vcoord_cesm
+
+    recursive subroutine read_3d_cesm(idate,v,lonlyc)
+      implicit none
+      type(rcm_time_and_date) , intent(in) :: idate
+      type(cmip6_3d_var) , pointer , intent(inout) :: v
+      logical , optional , intent(in) :: lonlyc
+      integer(ik4) :: istatus , idimid , it , irec
+      integer(ik4) :: year , month , day , hour , y1 , y2
+      character(len=32) :: timecal , timeunit
+      integer(ik4) , dimension(4) :: istart , icount
+      real(rk8) , dimension(2) :: times
+      type(rcm_time_interval) :: tdif
+      character(len=16) :: ver
+
+      if ( v%ncid == -1 ) then
+        call split_idate(idate, year, month, day, hour)
+        if ( year < 2010 ) then
+          if ( v%vname /= 'ta' ) then
+            ver = cesm_version
+          else
+            ver = cesm_version2
+          end if
+          y1 = year/10*10
+          y2 = y1+9
+          write(v%filename,'(a,i4,a,i4,a)') &
+            trim(cmip6_path(year,'6hrLev',ver,v%vname)), &
+            y1, '01010000-', y2, '12311800.nc'
+        else if ( year >= 2015 .and. year < 2095 ) then
+          y1 = (year-2015)/10*10 + 2015
+          y2 = y1+9
+          write(v%filename,'(a,i4,a,i4,a)') &
+            trim(cmip6_path(year,'6hrLev',cesm_version1,v%vname)), &
+            y1, '01010000-', y2, '12311800.nc'
+        else
+          if ( idate < 2015010100 ) then
+            if ( v%vname /= 'ta' ) then
+              ver = cesm_version
+            else
+              ver = cesm_version2
+            end if
+            write(v%filename,'(a,a)') &
+              trim(cmip6_path(year,'6hrLev',ver,v%vname)), &
+              '201001010000-201501010000.nc'
+          else
+            write(v%filename,'(a,a)') &
+              trim(cmip6_path(year,'6hrLev',cesm_version1,v%vname)), &
+              '209501010000-210101010000.nc'
+          end if
+        end if
+#ifdef DEBUG
+        write(stderr,*) 'Opening ',trim(v%filename)
+#endif
+        istatus = nf90_open(v%filename,nf90_nowrite,v%ncid)
+        call cmip6_error(istatus,__FILE__,__LINE__, &
+          'Error opening file '//trim(v%filename)//'.')
+      end if
+      if ( .not. associated(v%hcoord) ) then
+        allocate(v%hcoord)
+        call read_hcoord_cesm(v%ncid,v%hcoord%lon1d,v%hcoord%lat1d)
+      end if
+      if ( .not. associated(v%vcoord) ) then
+        allocate(v%vcoord)
+        call read_vcoord_cesm(v%ncid,v%vcoord%ak,v%vcoord%bk,v%vcoord%p0)
+      end if
+      if ( .not. associated(v%var) ) then
+        v%ni = size(v%hcoord%lon1d)
+        v%nj = size(v%hcoord%lat1d)
+        v%nk = size(v%vcoord%ak)
+        call getmem3d(v%var,1,v%ni,1,v%nj,1,v%nk,'cmip6:cesm:'//trim(v%vname))
+#ifdef DEBUG
+        write(stderr,*) 'Input shape for ',trim(v%vname),' = ', &
+          v%ni,'x',v%nj,'x',v%nk
+#endif
+        if ( present(lonlyc) ) then
+          if ( lonlyc ) return
+        end if
+      end if
+      if ( v%ivar == -1 ) then
+        istatus = nf90_inq_varid(v%ncid,v%vname,v%ivar)
+        call cmip6_error(istatus,__FILE__,__LINE__, &
+          'Error searchong '//trim(v%vname)//' var in file '// &
+          trim(v%filename)//'.')
+      end if
+      if ( v%nrec == -1 ) then
+        istatus = nf90_inq_dimid(v%ncid,'time',idimid)
+        call cmip6_error(istatus,__FILE__,__LINE__,'Error find time dim')
+        istatus = nf90_inquire_dimension(v%ncid,idimid,len=v%nrec)
+        call cmip6_error(istatus,__FILE__,__LINE__,'Error inquire time dim')
+        istatus = nf90_inq_varid(v%ncid,'time',it)
+        call cmip6_error(istatus,__FILE__,__LINE__, &
+          'Error searching time var in file '//trim(v%filename)//'.')
+        istatus = nf90_get_att(v%ncid,it,"calendar",timecal)
+        call cmip6_error(istatus,__FILE__,__LINE__, &
+          'Error reading time attribute calendar in file '//&
+          trim(v%filename)//'.')
+        istatus = nf90_get_att(v%ncid,it,"units",timeunit)
+        call cmip6_error(istatus,__FILE__,__LINE__, &
+          'Error reading time attribute units in file '//trim(v%filename)//'.')
+        istart(1) = 1
+        icount(1) = 2
+        istatus = nf90_get_var(v%ncid,it,times,istart(1:1),icount(1:1))
+        call cmip6_error(istatus,__FILE__,__LINE__, &
+          'Error reading time from file '//trim(v%filename)//'.')
+        v%first_date = timeval2date(times(1),timeunit,timecal)
+      end if
+
+      tdif = idate - v%first_date
+      irec = nint(tohours(tdif)/6.0) + 1
+
+      if ( irec > v%nrec ) then
+        istatus = nf90_close(v%ncid)
+        call cmip6_error(istatus,__FILE__,__LINE__, &
+          'Error close file '//trim(v%filename)//'.')
+        v%ncid = -1
+        v%ivar = -1
+        v%nrec = -1
+        irec = 1
+#ifdef DEBUG
+        write(stderr, *) 'Closed file, switching to the next in series...'
+#endif
+        call read_3d_cesm(idate,v)
+      end if
+      istart(1) = 1
+      istart(2) = 1
+      istart(3) = 1
+      istart(4) = irec
+      icount(1) = size(v%var,1)
+      icount(2) = size(v%var,2)
+      icount(3) = size(v%var,3)
+      icount(4) = 1
+      istatus = nf90_get_var(v%ncid,v%ivar,v%var,istart,icount)
+      call cmip6_error(istatus,__FILE__,__LINE__, &
+          'Error read variable '//v%vname//' from '//trim(v%filename)//'.')
+    end subroutine read_3d_cesm
+
+    recursive subroutine read_2d_cesm(idate,v,lonlyc)
+      implicit none
+      type(rcm_time_and_date) , intent(in) :: idate
+      type(cmip6_2d_var) , pointer , intent(inout) :: v
+      logical , optional , intent(in) :: lonlyc
+      integer(ik4) :: istatus , idimid , it , irec
+      integer(ik4) :: y1 , y2
+      integer(ik4) :: year , month , day , hour , y
+      character(len=32) :: timecal , timeunit
+      integer(ik4) , dimension(3) :: istart , icount
+      real(rk8) , dimension(2) :: times
+      type(rcm_time_interval) :: tdif
+
+      if ( v%ncid == -1 ) then
+        call split_idate(idate, year, month, day, hour)
+        if ( year < 2010 ) then
+          y1 = year/10 * 10
+          write(v%filename,'(a,i4,a,i4,a)') &
+            trim(cmip6_path(year,'6hrLev',cesm_version,v%vname)), &
+            y1,'01010000-',y1+9,'12311800.nc'
+        else if ( year >= 2015 .and. year < 2095 ) then
+          y1 = (year-2015)/10*10 + 2015
+          write(v%filename,'(a,i4,a,i4,a)') &
+            trim(cmip6_path(year,'6hrLev',cesm_version1,v%vname)), &
+            y1,'01010000-',y1+9,'12311800.nc'
+        else
+          if ( idate < 2015010100 ) then
+            write(v%filename,'(a,a)') &
+              trim(cmip6_path(year,'6hrLev',cesm_version,v%vname)), &
+              '201001010000-201501010000.nc'
+          else
+            write(v%filename,'(a,a)') &
+              trim(cmip6_path(year,'6hrLev',cesm_version1,v%vname)), &
+              '209501010000-210101010000.nc'
+          end if
+        end if
+#ifdef DEBUG
+        write(stderr,*) 'Opening ',trim(v%filename)
+#endif
+        istatus = nf90_open(v%filename,nf90_nowrite,v%ncid)
+        call cmip6_error(istatus,__FILE__,__LINE__, &
+          'Error opening file '//trim(v%filename)//'.')
+      end if
+      if ( .not. associated(v%hcoord) ) then
+        allocate(v%hcoord)
+        call read_hcoord_cesm(v%ncid,v%hcoord%lon1d,v%hcoord%lat1d)
+      end if
+      if ( .not. associated(v%var) ) then
+        v%ni = size(v%hcoord%lon1d)
+        v%nj = size(v%hcoord%lat1d)
+        call getmem2d(v%var,1,v%ni,1,v%nj,'cmip6:cesm:'//trim(v%vname))
+#ifdef DEBUG
+        write(stderr,*) 'Input shape for ',trim(v%vname),' = ',v%ni,'x',v%nj
+#endif
+        if ( present(lonlyc) ) then
+          if ( lonlyc ) return
+        end if
+      end if
+      if ( v%ivar == -1 ) then
+        istatus = nf90_inq_varid(v%ncid,v%vname,v%ivar)
+        call cmip6_error(istatus,__FILE__,__LINE__, &
+          'Error searchong '//trim(v%vname)//' var in file '// &
+          trim(v%filename)//'.')
+      end if
+      if ( v%nrec == -1 ) then
+        istatus = nf90_inq_dimid(v%ncid,'time',idimid)
+        call cmip6_error(istatus,__FILE__,__LINE__,'Error find time dim')
+        istatus = nf90_inquire_dimension(v%ncid,idimid,len=v%nrec)
+        call cmip6_error(istatus,__FILE__,__LINE__,'Error inquire time dim')
+        istatus = nf90_inq_varid(v%ncid,'time',it)
+        call cmip6_error(istatus,__FILE__,__LINE__, &
+          'Error searching time var in file '//trim(v%filename)//'.')
+        istatus = nf90_get_att(v%ncid,it,"calendar",timecal)
+        call cmip6_error(istatus,__FILE__,__LINE__, &
+          'Error reading time attribute calendar in file '//&
+          trim(v%filename)//'.')
+        istatus = nf90_get_att(v%ncid,it,"units",timeunit)
+        call cmip6_error(istatus,__FILE__,__LINE__, &
+          'Error reading time attribute units in file '//trim(v%filename)//'.')
+        istart(1) = 1
+        icount(1) = 2
+        istatus = nf90_get_var(v%ncid,it,times,istart(1:1),icount(1:1))
+        call cmip6_error(istatus,__FILE__,__LINE__, &
+          'Error reading time from file '//trim(v%filename)//'.')
+        v%first_date = timeval2date(times(1),timeunit,timecal)
+      end if
+
+      tdif = idate - v%first_date
+      irec = nint(tohours(tdif)/6.0) + 1
+
+      if ( irec > v%nrec ) then
+        istatus = nf90_close(v%ncid)
+        call cmip6_error(istatus,__FILE__,__LINE__, &
+          'Error close file '//trim(v%filename)//'.')
+        v%ncid = -1
+        v%ivar = -1
+        v%nrec = -1
+        irec = 1
+#ifdef DEBUG
+        write(stderr, *) 'Closed file, switching to the next in series...'
+#endif
+        call read_2d_cesm(idate,v)
+      end if
+      istart(1) = 1
+      istart(2) = 1
+      istart(3) = irec
+      icount(1) = size(v%var,1)
+      icount(2) = size(v%var,2)
+      icount(3) = 1
+      istatus = nf90_get_var(v%ncid,v%ivar,v%var,istart,icount)
+      call cmip6_error(istatus,__FILE__,__LINE__, &
+          'Error read variable '//v%vname//' from '//trim(v%filename)//'.')
+    end subroutine read_2d_cesm
+
+    recursive subroutine read_fx_cesm(v)
+      implicit none
+      type(cmip6_2d_var) , pointer , intent(inout) :: v
+      integer(ik4) :: istatus
+
+      v%filename = trim(cmip6_fxpath(cesm_version,v%vname))
+#ifdef DEBUG
+      write(stderr,*) 'Opening ',trim(v%filename)
+#endif
+      istatus = nf90_open(v%filename,nf90_nowrite,v%ncid)
+      call cmip6_error(istatus,__FILE__,__LINE__, &
+          'Error opening file '//trim(v%filename)//'.')
+      allocate(v%hcoord)
+      call read_hcoord_hadmm(v%ncid,v%hcoord%lon1d,v%hcoord%lat1d)
+      v%ni = size(v%hcoord%lon1d)
+      v%nj = size(v%hcoord%lat1d)
+      call getmem2d(v%var,1,v%ni,1,v%nj,'cmip6:cesm:'//trim(v%vname))
+#ifdef DEBUG
+      write(stderr,*) 'Input shape for ',trim(v%vname),' = ',v%ni,'x',v%nj
+#endif
+      istatus = nf90_inq_varid(v%ncid,v%vname,v%ivar)
+      call cmip6_error(istatus,__FILE__,__LINE__, &
+        'Error searchong '//trim(v%vname)//' var in file '// &
+        trim(v%filename)//'.')
+      istatus = nf90_get_var(v%ncid,v%ivar,v%var)
+      call cmip6_error(istatus,__FILE__,__LINE__, &
+          'Error read variable '//v%vname//' from '//trim(v%filename)//'.')
+      istatus = nf90_close(v%ncid)
+      call cmip6_error(istatus,__FILE__,__LINE__, &
+          'Error close file '//trim(v%filename)//'.')
+    end subroutine read_fx_cesm
 
 end module mod_cmip6
 
