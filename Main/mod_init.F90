f571b36b36de91f3f96d6bbefba519080dac53f4
Graziano Giuliani <ggiulian@ictp.it>
2022-03-07 13:36:59 +0100

    fix RRTM top

--- Main/mod_init.F90
+++ Main/mod_init.F90
@@ -308,7 +308,8 @@ module mod_init
         end do
         do i = ice1 , ice2
           do j = jce1 , jce2
-            mo_atm%pf(j,i,1) = p00 * (d_half*mo_atm%pai(j,i,1))**cpovr
+            mo_atm%pf(j,i,1) = mo_atm%p(j,i,1) - egrav * mo_atm%rho(j,i,1) * &
+              (mo_atm%zetaf(j,i,1)-mo_atm%zeta(j,i,1))
           end do
         end do
 
@@ -926,7 +927,8 @@ module mod_init
       ! Top pressure should be zero.
       do i = ice1 , ice2
         do j = jce1 , jce2
-          mo_atm%pf(j,i,1) = p00 * (d_half*mo_atm%pai(j,i,1))**cpovr
+          mo_atm%pf(j,i,1) = mo_atm%p(j,i,1) - egrav * mo_atm%rho(j,i,1) * &
+            (mo_atm%zetaf(j,i,1)-mo_atm%zeta(j,i,1))
         end do
       end do
     end if
