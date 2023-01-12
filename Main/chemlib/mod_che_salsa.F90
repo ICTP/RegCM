efc3b14abb76e6983b0ea8de4d077ec79f43fc0a
fsolmon <fabien.solmon@aero.obs-mip.fr>
2023-01-12 13:02:14 +0100

    salsa dev

--- Main/chemlib/mod_che_salsa.F90
+++ Main/chemlib/mod_che_salsa.F90
@@ -23,7 +23,7 @@ module mod_che_salsa
   use mod_realkinds
   use mod_dynparam
   use mod_constants
-  use mod_runparams , only : dtsec 
+  use mod_runparams , only : dtsec, dtche 
   !use mod_runparams , only : rcmtimer
   use mod_che_common
   use mod_che_indices
@@ -36,7 +36,7 @@ module mod_che_salsa
   USE mo_salsa, ONLY : salsa
   USE mo_salsa_init
   USE mo_salsa_properties, ONLY : equilibration
-  USE class_componentIndex, ONLY : ComponentIndex, GetIndex, GetNcomp, IsUsed
+  USE class_componentIndex, ONLY : ComponentIndex, GetIndex, GetNcomp, IsUsed, ComponentIndexConstructor
 
   implicit none
 
@@ -46,6 +46,7 @@ module mod_che_salsa
   INTEGER(ik4)  :: kbdim 
   INTEGER(ik4)  :: klev 
   INTEGER(ik4)  :: krow
+  integer(ik4),parameter :: n4 = 4
 
 !  REAL(rkx), PARAMETER :: init_rh(kbdim,klev) = 0.3_dp
 
@@ -55,7 +56,10 @@ module mod_che_salsa
   TYPE(t_section), ALLOCATABLE, SAVE :: precp(:,:,:) ! Precipitation properties
 
   TYPE(ComponentIndex) :: prtcl ! Contains "getIndex" which gives the index for a
-
+                                ! aerosol component name, i.e. 1:SO4, 2:OC, 3:BC, 4:DU,
+                                ! 5:SS, 6:NO, 7:NH, 8:H2O
+  
+  CHARACTER(len=3) :: complist(n4)
 
   ! -- Local gas compound tracers [# m-3]
   REAL(rkx), allocatable :: zgso4(:,:),   &
@@ -81,19 +85,39 @@ module mod_che_salsa
     kproma = (jci2-jci1+1)*(ici2-ici1+1)
     kbdim = kproma
     klev = kz 
+
+    ! FAB setup chemical components, no organics for now but could be 
+    complist(1)='SO4'
+    complist(2)='NO'
+    complist(3)='NH'
+    complist(4)='H2O'
+
+    CALL ComponentIndexConstructor(prtcl, n4, 4, complist)
+
+
     allocate(in_t(1:kbdim,1:klev))    
     allocate(in_p(1:kbdim,1:klev))    
     allocate(in_rv(1:kbdim,1:klev))   
     allocate(in_rs(1:kbdim,1:klev))   
     allocate(in_w(1:kbdim,1:klev)) 
-    
+    allocate(zgso4(1:kbdim,1:klev))   
+    allocate(zghno3(1:kbdim,1:klev))  
+    allocate(zgnh4(1:kbdim,1:klev))   
+    allocate(zgocnv(1:kbdim,1:klev))  
+    allocate(zgocsv(1:kbdim,1:klev))  
+
     call salsa_initialize()
-  end subroutine init_salsa
+  end subroutine init_salsa  
+
+
 
   subroutine run_salsa()
     implicit none
+
     integer(ik4) :: i,j,k,n,nc,vc
 ! set input
+
+  
    do k = 1 , kz
     n = 1
     do i = ici1 , ici2
@@ -107,9 +131,11 @@ module mod_che_salsa
         IF (IsUsed(prtcl,'SO4')) THEN
                 nc = GetIndex(prtcl,'SO4')
                 vc = 1
-             !   str = (nc-1)*nbins+1
+                print*, 'zozo', nc,nbins
+                !   str = (nc-1)*nbins+1
              !   end = nc*nbins
-                aero(n,k,1:nbins)%volc(vc) = 0.
+                aero(n,k,1:nbins)%volc(vc) = 0._rkx
+                print*,'zozend'
         END IF
 
         n = n + 1
@@ -117,7 +143,7 @@ module mod_che_salsa
     end do
    end do
 
-   print* , 'FAB', in_t (50, kz), in_p(50,kz), in_rv(50,kz),in_rs(50,kz), in_w(50,kz) 
+!   print* , 'FAB', in_t (50, kz), in_p(50,kz), in_rv(50,kz),in_rs(50,kz), in_w(50,kz) 
  
 
 !    actd(1:kproma,:,:)%numc = 0._dp
