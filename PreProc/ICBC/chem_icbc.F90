fa667f43d8c52e9f8ebf5e88970d75cbf4996eb2
Graziano Giuliani <ggiulian@ictp.it>
2023-10-11 18:04:15 +0200

    fix missing parameter

--- PreProc/ICBC/chem_icbc.F90
+++ PreProc/ICBC/chem_icbc.F90
@@ -68,11 +68,11 @@ program chem_icbc
         isnowdark , ichecold
   logical :: carb_aging_control
   integer(ik4) :: ichem , iclimaaer
-  integer(ik4) ibltyp , iboudy , isladvec , iqmsl , icup_lnd , icup_ocn , &
-    ipgf , iemiss , lakemod , ipptls , iocnflx , iocncpl , iwavcpl ,      &
-    iocnrough , iocnzoq , idcsst , iseaice , idesseas , iconvlwp ,        &
-    icldmstrat , icldfrac , irrtm , iclimao3 , isolconst , icumcloud ,    &
-    islab_ocean , itweak , ghg_year_const , idiffu , icopcpl ,            &
+  integer(ik4) ibltyp , iboudy , isladvec , iqmsl , icup_lnd , icup_ocn ,  &
+    ipgf , iemiss , lakemod , ipptls , iocnflx , iocncpl , iwavcpl ,       &
+    iocnrough , iocnzoq , idcsst , iseaice , idesseas , iconvlwp ,         &
+    ioasiscpl , icldmstrat , icldfrac , irrtm , iclimao3 , isolconst ,     &
+    icumcloud , islab_ocean , itweak , ghg_year_const , idiffu , icopcpl , &
     iwhitecap , ifixsolar , year_offset , ichdustparam , irceideal
   real(rkx) :: temp_tend_maxval , wind_tend_maxval , fixedsolarval
   character(len=8) :: scenario
