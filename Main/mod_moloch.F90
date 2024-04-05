9dabd0b1800ef490ad7f8575a17e6acffbc10899
Graziano Giuliani <ggiulian@ictp.it>
2024-04-05 17:37:02 +0200

    avoid fpes

--- Main/mod_moloch.F90
+++ Main/mod_moloch.F90
@@ -1702,7 +1702,9 @@ module mod_moloch
         do concurrent ( j = jci1:jci2, i = ici1:ici2, &
                         k = 1:kz, n = iqfrst:nqx)
           qx(j,i,k,n) = qx(j,i,k,n) + mo_atm%qxten(j,i,k,n)*dtsec
-          qx(j,i,k,n) = max(qx(j,i,k,n),d_zero)
+          if ( qx(j,i,k,n) < 1.0e-20_rkx ) then
+            qx(j,i,k,n) = d_zero
+          end if
         end do
         if ( ibltyp == 2 ) then
           do concurrent ( j = jci1:jci2, i = ici1:ici2, k = 1:kzp1 )
@@ -1712,7 +1714,9 @@ module mod_moloch
         if ( ichem == 1 ) then
           do concurrent ( j = jci1:jci2, i = ici1:ici2, k = 1:kz, n = 1:ntr )
             trac(j,i,k,n) = trac(j,i,k,n) + dtsec * mo_atm%chiten(j,i,k,n)
-            trac(j,i,k,n) = max(trac(j,i,k,n),d_zero)
+            if ( trac(j,i,k,n) < 1.0e-50_rkx ) then
+              trac(j,i,k,n) = d_zero
+            end if
           end do
         end if
 #ifdef DEBUG
