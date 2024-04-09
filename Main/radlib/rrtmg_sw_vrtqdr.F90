ffa1410faa786e81560da394939871823d16efb4
Graziano Giuliani <ggiulian@ictp.it>
2024-04-09 17:25:53 +0200

    backport

--- Main/radlib/rrtmg_sw_vrtqdr.F90
+++ Main/radlib/rrtmg_sw_vrtqdr.F90
@@ -102,7 +102,7 @@
 
       integer(kind=im) :: ikp, ikx, jk
 
-      real(kind=rb) :: zreflect, t1, t2
+      real(kind=rb) :: zreflect
       real(kind=rb) :: ztdn(klev+1)
 
 ! Definitions
@@ -151,24 +151,9 @@
       do jk = 2,klev
          ikp = jk+1
          zreflect = 1._rb / (1._rb - prefd(jk) * prdnd(jk))
-         if ( all([ptdbt(jk),ptra(jk)] > 1.0e-20_rb) ) then
-           t1 = ptdbt(jk) * ptra(jk)
-         else
-           t1 = 0.0_rb
-         end if
-         t2 = ((ztdn(jk) - ptdbt(jk)) + &
-               ptdbt(jk) * pref(jk) * prdnd(jk))
-         if ( all([ptrad(jk),t2] > 1.0e-20_rb) ) then
-           t2 = ptrad(jk) * t2
-         else
-           t2 = 0.0_rb
-         end if
-         if ( all([t2,zreflect] > 1.0e-20_rb) ) then
-           t2 = t2 * zreflect
-         else
-           t2 = 0.0_rb
-         end if
-         ztdn(ikp) = t1 + t2
+         ztdn(ikp) = (ptdbt(jk) * ptra(jk) + &
+                      ptrad(jk) * ((ztdn(jk) - ptdbt(jk)) + &
+                      ptdbt(jk) * pref(jk) * prdnd(jk)) * zreflect)
          prdnd(ikp) = prefd(jk) + ptrad(jk) * ptrad(jk) * &
                       prdnd(jk) * zreflect
       enddo
