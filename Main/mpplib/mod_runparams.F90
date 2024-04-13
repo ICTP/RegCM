4ba9f5ec8ff5227e8da18d98445179c843cd6bee
Graziano Giuliani <ggiulian@ictp.it>
2024-04-13 13:48:45 +0200

    merge 19

--- Main/mpplib/mod_runparams.F90
+++ Main/mpplib/mod_runparams.F90
@@ -141,6 +141,7 @@ module mod_runparams
   integer(ik4) , public :: lakemod
   ! Diurnal cycle SST index
   integer(ik4) , public :: idcsst
+  integer(ik4) , public :: iwhitecap
   ! Sea Ice scheme index
   integer(ik4) , public :: iseaice
   real(rkx) , public :: icetriggert
