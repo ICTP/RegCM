e09bb3d94d68b45a45b907e189470d169bd6bc20
Quentin Desmet <desmetque@gmail.com>
2024-07-18 13:47:50 +0200

    chore(OASIS): remove commented calls to deleted functions

--- Main/mod_regcm_interface.F90
+++ Main/mod_regcm_interface.F90
@@ -150,12 +150,6 @@ module mod_regcm_interface
     !
 #ifdef OASIS
     if ( ioasiscpl == 1 ) then
-#ifdef DEBUG
-      !
-      ! OASIS Log Files Setup
-      !
-      !call oasisxregcm_open_log(comp_name,comp_id)
-#endif
       !
       ! OASIS Variables Setup
       !
@@ -363,12 +357,6 @@ module mod_regcm_interface
       ! OASIS Variables Release
       !
       call oasisxregcm_release
-#ifdef DEBUG
-      !
-      ! OASIS Log Files Closing
-      !
-      !call oasisxregcm_close_log
-#endif
     end if
 #endif
 
