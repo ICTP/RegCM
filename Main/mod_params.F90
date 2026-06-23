d3e1634b322ec6d8c0126f47b48095ff4fba074f
Graziano Giuliani <ggiulian@ictp.it>
2026-06-23 17:06:37 +0200

    off by one was not yet fixed. now it is.

--- Main/mod_params.F90
+++ Main/mod_params.F90
@@ -3305,20 +3305,22 @@ module mod_params
         call exchange_lrbt(mddom%msfv,1,jde1,jde2,ide1,ide2)
         call exchange(mddom%htu,2,jde1,jde2,ide1,ide2)
         call exchange(mddom%htv,2,jde1,jde2,ide1,ide2)
-        do concurrent ( j = jdi1ga:jdi2ga, i = ici1:ice2 )
+        do concurrent ( j = jdi1:jdi2, i = ici1:ici2 )
           mddom%hx(j,i) = (mddom%ht(j,i) * mddom%msfx(j,i) - &
                   mddom%ht(j-1,i) * mddom%msfx(j-1,i)) * rdx * regrav
         end do
         if ( iproj == 'ROTLLR' ) then
-          do concurrent ( j = jci1:jce2, i = idi1ga:idi2ga )
+          do concurrent ( j = jci1:jce2, i = idi1:idi2 )
             mddom%hy(j,i) = (mddom%ht(j,i) - mddom%ht(j,i-1)) * rdx * regrav
           end do
         else
-          do concurrent ( j = jci1:jce2, i = idi1ga:idi2ga )
+          do concurrent ( j = jci1:jci2, i = idi1:idi2 )
             mddom%hy(j,i) = (mddom%ht(j,i) * mddom%msfx(j,i) - &
                   mddom%ht(j,i-1) * mddom%msfx(j,i-1)) * rdx * regrav
           end do
         end if
+        call exchange_lr(mddom%hx,1,jde1,jde2,ice1,ice2)
+        call exchange_bt(mddom%hy,1,jce1,jce2,ide1,ide2)
         do concurrent ( j = jce1:jce2, i = ice1:ice2, k = 1:kz )
           mo_atm%zeta(j,i,k) = md_zeta(zitah(k), &
                       mddom%ht(j,i),mo_ztop,mo_h,mo_a0)
